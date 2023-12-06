9dcc3ebff5af668c5235641fea9d36efd283240b
MrSom3body <karunsandhu@proton.me>
2023-12-06 15:19:48 +0100

    Add fish alias to start xampp

---
+++ .config/fish/functions/xampp.fish
@@ -0,0 +1,4 @@
+function xampp --wraps='sudo /opt/lampp/manager-linux-x64.run' --description 'alias xampp=sudo /opt/lampp/manager-linux-x64.run'
+  sudo /opt/lampp/manager-linux-x64.run $argv
+        
+end

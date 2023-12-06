8b8095e6d62ccf1d80a26b4d957bc5ba25d5b759
MrSom3body <karunsandhu@proton.me>
2023-12-06 14:36:11 +0100

    Replace exa with eza in ls fish alias

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps='exa --group-directories-first' --wraps='exa -g --group-directories-first' --wraps='eza -g --group-directories-first' --description 'alias ls=eza -g --group-directories-first'
+function ls --wraps='eza -g --group-directories-first' --description 'alias ls=eza -g --group-directories-first'
   eza -g --group-directories-first $argv
         
 end

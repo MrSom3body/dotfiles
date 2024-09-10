2cf8a26805883d97490e63e5c6017506e194e0dd
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-09-10 12:12:20 +0200

    Add markdown support to helix

--- home/editors/helix/languages.nix
+++ home/editors/helix/languages.nix
@@ -31,12 +31,24 @@
       vscode-css-language-server = {
         command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-css-languageserver";
       };
+
+      marksman = {
+        command = lib.getExe pkgs.marksman;
+      };
     };
 
     language = [
       {
         name = "nix";
         language-servers = ["nixd"];
+        auto-format = true;
+      }
+      {
+        name = "markdown";
+        formatter = {
+          command = lib.getExe pkgs.nodePackages.prettier;
+          args = ["--parser" "markdown"];
+        };
         auto-format = true;
       }
     ];

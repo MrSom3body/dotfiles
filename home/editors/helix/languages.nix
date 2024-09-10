bcaa54f5976948e5f2839f3fbfd36d3db35a77c3
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-09-10 22:46:13 +0200

    Refactor helix languages config

--- home/editors/helix/languages.nix
+++ home/editors/helix/languages.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   pkgs,
   dotfiles,
@@ -7,23 +6,17 @@
 }: {
   programs.helix.languages = {
     language-server = {
-      # nil = {
-      #   command = lib.getExe pkgs.nil;
-      #   config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
-      # };
+      marksman = {
+        command = lib.getExe pkgs.marksman;
+      };
+
       nixd = {
         command = lib.getExe pkgs.nixd;
         config.nixd = {
           formatting = lib.getExe pkgs.alejandra;
-
           options = {
-            nixos = {
-              expr = "(builtins.getFlake \"${dotfiles.path}\").nixosConfigurations.${dotfiles.hostname}.options";
-            };
-
-            home-manager = {
-              expr = "(builtins.getFlake \"${dotfiles.path}\").homeManagerConfigurations.${dotfiles.username}.options";
-            };
+            nixos = {expr = "(builtins.getFlake \"${dotfiles.path}\").nixosConfigurations.${dotfiles.hostname}.options";};
+            home-manager = {expr = "(builtins.getFlake \"${dotfiles.path}\").homeManagerConfigurations.\"${dotfiles.username}\".options";};
           };
         };
       };
@@ -32,23 +25,27 @@
         command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-css-languageserver";
       };
 
-      marksman = {
-        command = lib.getExe pkgs.marksman;
+      vscode-html-language-server = {
+        command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-html-languageserver";
+      };
+
+      vscode-json-language-server = {
+        command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-json-languageserver";
       };
     };
 
     language = [
-      {
-        name = "nix";
-        language-servers = ["nixd"];
-        auto-format = true;
-      }
       {
         name = "markdown";
         formatter = {
           command = lib.getExe pkgs.nodePackages.prettier;
           args = ["--parser" "markdown"];
         };
+        auto-format = true;
+      }
+      {
+        name = "nix";
+        language-servers = ["nixd"];
         auto-format = true;
       }
     ];

cd24be8bd22346077abf90641179d0815944335f
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-09-20 15:30:20 +0200

    system/services/gns3.nix: gns3 thinks it's funny

--- system/services/gns3.nix
+++ system/services/gns3.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
+  services.gns3-server = {
+    enable = true;
+    dynamips.enable = true;
+    ubridge.enable = true;
+    vpcs.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     gns3-gui
-    gns3-server
+    inetutils
   ];
 }

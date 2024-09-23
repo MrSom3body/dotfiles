de50998f00e3f4e0b33a93b35fa2bb6bd4160973
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-09-23 12:21:20 +0200

    home/terminal/programs/btop.nix: add nvidia support

--- home/terminal/programs/btop.nix
+++ home/terminal/programs/btop.nix
@@ -1,6 +1,10 @@
-{
+{pkgs, ...}: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override {
+      cudaSupport = true;
+    };
+
     settings = {
       update_ms = 700;
       vim_keys = true;

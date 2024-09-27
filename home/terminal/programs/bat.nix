a74fd77a50e7afa1b7d0b7554ec8dfbe8f010206
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-09-27 07:39:51 +0200

    terminal/bat: remove unused bat commands

--- home/terminal/programs/bat.nix
+++ home/terminal/programs/bat.nix
@@ -2,20 +2,13 @@
   programs.bat = {
     enable = true;
     extraPackages = with pkgs.bat-extras; [
-      batgrep
       batman
-      batpipe
-      batwatch
-      # batdiff
       prettybat
     ];
   };
 
   home.shellAliases = {
     cat = "bat";
-    # rg = "batgrep";
     man = "batman";
-    less = "batpipe";
-    # diff = "batdiff";
   };
 }

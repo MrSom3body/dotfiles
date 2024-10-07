7748fa84d6e2ce78127b248c5d4001cac39c3e4d
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-07 23:16:27 +0200

    greetd: enable regreet

--- system/services/greetd.nix
+++ system/services/greetd.nix
@@ -3,18 +3,19 @@
   lib,
   ...
 }: {
-  services.greetd = let
-    session = {
-      command = "${lib.getExe config.programs.hyprland.package}";
-      user = "karun";
-    };
-  in {
+  services.greetd = {
     enable = true;
     settings = {
       terminal.vt = 1;
-      default_session = session;
-      initial_session = session;
+      initial_session = {
+        command = "${lib.getExe config.programs.hyprland.package}";
+        user = "karun";
+      };
     };
+  };
+
+  programs.regreet = {
+    enable = true;
   };
 
   # unlock GPG keyring on login

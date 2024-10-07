e5e10dae097ecd33745c456c85be5866dd568d9b
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-07 23:46:53 +0200

    stylix: disable some targets

--- home/style/stylix.nix
+++ home/style/stylix.nix
@@ -2,7 +2,9 @@
   stylix = {
     targets = {
       kitty.variant256Colors = true;
+      neovim.enable = false;
       rofi.enable = false; # I have a custom rofi theme
+      spicetify.enable = false;
       waybar.enable = false; # I have a custom waybar theme
     };
   };

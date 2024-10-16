27853c6a54ba83865fa84cc7b70c0bd4c595ed87
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-16 22:40:34 +0200

    kitty: reformat

--- home/terminal/emulators/kitty.nix
+++ home/terminal/emulators/kitty.nix
@@ -5,16 +5,19 @@
 }: {
   programs.kitty = {
     enable = true;
+
     font = {
       inherit (config.stylix.fonts.monospace) name;
       size = config.stylix.fonts.sizes.terminal;
     };
+
     shellIntegration = {
       mode = "no-sudo";
       enableBashIntegration = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
     };
+
     settings = {
       background_opacity = lib.mkForce 1;
       touch_scroll_multiplier = "5.0";

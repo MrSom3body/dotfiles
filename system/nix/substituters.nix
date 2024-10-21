2d00a59fc5af5a98bef382b3f27a6fbf9772c888
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-21 10:30:17 +0200

    nix/substituters: add cuda-maintainers cache

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -3,14 +3,16 @@
     substituters = [
       "https://cache.nixos.org?priority=10"
 
-      "https://hyprland.cachix.org"
+      "https://cuda-maintainers.cachix.org"
       "https://helix.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }

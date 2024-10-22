78ea9ca395dffe1159fa25f597aec680e3445a65
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-22 14:35:24 +0200

    shell: remove alias for ollama

--- home/terminal/shell/default.nix
+++ home/terminal/shell/default.nix
@@ -1,8 +1,4 @@
 {
-  lib,
-  dotfiles,
-  ...
-}: {
   imports = [
     ./bash.nix
     ./fish
@@ -14,7 +10,6 @@
   home.shellAliases = {
     ip = "ip -c";
     l = "ls";
-    chat = lib.mkIf ((builtins.length dotfiles.ollamaModels) == 1) "ollama run llama3.2";
     mkdev = "nix flake new -t \"github:numtide/devshell\"";
   };
 

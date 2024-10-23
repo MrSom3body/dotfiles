8e0a99ba458e583f084ad0bcfeb51f4843015f24
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-23 09:12:16 +0200

    ollama: enable nvidia_uvm kernel module

--- system/services/ollama.nix
+++ system/services/ollama.nix
@@ -1,7 +1,13 @@
 {
+  config,
+  lib,
+  ...
+}: {
   services.ollama = {
     enable = true;
     acceleration = "cuda";
     loadModels = ["llama3.2" "llama2-uncensored" "llava"];
   };
+
+  boot.kernelModules = lib.mkIf config.services.ollama.enable ["nvidia_uvm"];
 }

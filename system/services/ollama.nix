8c6b6f448daa9f09bf621767900ebac2586b4c39
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-24 11:15:05 +0200

    system: add specialisation for ollama with GPU support

--- system/services/ollama.nix
+++ system/services/ollama.nix
@@ -5,9 +5,12 @@
 }: {
   services.ollama = {
     enable = true;
-    acceleration = "cuda";
     loadModels = ["llama3.2:3b-instruct-q4_K_M" "llava"];
   };
 
-  boot.kernelModules = lib.mkIf config.services.ollama.enable ["nvidia_uvm"];
+  specialisation.ollamaGPU.configuration = {
+    environment.etc."specialisation".text = "ollamaGPU";
+    services.ollama.acceleration = "cuda";
+    boot.kernelModules = lib.mkIf config.services.ollama.enable ["nvidia_uvm"];
+  };
 }

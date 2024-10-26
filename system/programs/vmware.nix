fe1d1f6624c4ed411a1e5e1517fd5cd5ab917423
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-10-27 00:51:38 +0200

    vmware: switch to unstable pkg

--- system/programs/vmware.nix
+++ system/programs/vmware.nix
@@ -1,7 +1,6 @@
-{pkgs-stable, ...}: {
+{
   virtualisation.vmware.host = {
     enable = true;
-    package = pkgs-stable.vmware-workstation;
     extraConfig = ''
       # Allow unsupported device's OpenGL and Vulkan acceleration for guest vGPU
       mks.gl.allowUnsupportedDrivers = "TRUE"

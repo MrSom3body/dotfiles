469dc6ef89b3f92bef8e763469d23467cf4abe41
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-13 09:12:51 +0100

    xdg: enable userDirs management

--- home/terminal/programs/xdg.nix
+++ home/terminal/programs/xdg.nix
@@ -1,17 +1,41 @@
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/pdf" = ["org.gnome.Papers.desktop"]; # PDFs
-      "application/zip" = ["org.gnome.Nautilus.desktop" "org.gnome.FileRoller.desktop"]; # ZIPs
-      "image/*" = ["org.gnome.Loupe.desktop"]; # Images
-      "image/jpg" = ["org.gnome.Loupe.desktop"]; # Images
-      "image/jpeg" = ["org.gnome.Loupe.desktop"]; # Images
-      "image/png" = ["org.gnome.Loupe.desktop"]; # Images
-      "text/plain" = ["Helix.desktop"]; # Plain text
-      "x-scheme-handler/http" = ["firefox.desktop"]; # Links
-      "x-scheme-handler/https" = ["firefox.desktop"]; # Links
-      "x-scheme-handler/mailto" = ["firefox.desktop"]; # Links
+  config,
+  pkgs,
+  dotfiles,
+  ...
+}: {
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = ["org.gnome.Papers.desktop"]; # PDFs
+        "application/zip" = ["org.gnome.Nautilus.desktop" "org.gnome.FileRoller.desktop"]; # ZIPs
+        "image/*" = ["org.gnome.Loupe.desktop"]; # Images
+        "image/jpeg" = ["org.gnome.Loupe.desktop"]; # Images
+        "image/jpg" = ["org.gnome.Loupe.desktop"]; # Images
+        "image/png" = ["org.gnome.Loupe.desktop"]; # Images
+        "inode/directory" = ["yazi"]; # Files
+        "text/plain" = ["Helix.desktop"]; # Plain text
+        "x-scheme-handler/http" = ["firefox.desktop"]; # Links
+        "x-scheme-handler/https" = ["firefox.desktop"]; # Links
+        "x-scheme-handler/mailto" = ["firefox.desktop"]; # Links
+      };
+    };
+
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      extraConfig = {
+        XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
+      };
     };
   };
+
+  home.packages = [
+    # used by `gio open` and xdp-gtk
+    (pkgs.writeShellScriptBin "xdg-terminal-exec" ''
+      ${dotfiles.terminal} "$@"
+    '')
+    pkgs.xdg-utils
+  ];
 }

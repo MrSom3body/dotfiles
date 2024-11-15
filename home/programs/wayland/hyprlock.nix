2383c3115de693cbef0b2f58e6d4355a9bc133a6
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-15 12:37:49 +0100

    flake.lock: update

--- home/programs/wayland/hyprlock.nix
+++ home/programs/wayland/hyprlock.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   inputs,
   pkgs,
   ...
@@ -16,7 +17,7 @@
       };
 
       background = {
-        path = "${config.stylix.image}";
+        path = lib.mkForce "${config.stylix.image}";
         blur_size = 6;
         blur_passes = 2;
       };
@@ -28,15 +29,11 @@
         dots_spacing = 0.15;
         dots_center = true;
         dots_rounding = -1;
-        outer_color = "rgb(${config.lib.stylix.colors.base0D})";
-        inner_color = "rgb(${config.lib.stylix.colors.base00})";
-        font_color = "rgb(${config.lib.stylix.colors.base05})";
         fade_on_empty = false;
         fade_timeout = 1000;
         placeholder_text = "<span foreground=\"##${config.lib.stylix.colors.base05}\">󰌾  Logged in as <span foreground=\"##${config.lib.stylix.colors.base0D}\"><i>$USER</i></span></span>";
         hide_input = false;
         rounding = -1;
-        fail_color = "rgb(${config.lib.stylix.colors.base08})";
         fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
         fail_transition = 300;
         capslock_color = "rgb(${config.lib.stylix.colors.base0A})";

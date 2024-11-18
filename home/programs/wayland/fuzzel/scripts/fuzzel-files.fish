a0d2b4b92dfca4f7d12113792c9f1e6f881a0b84
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-18 18:59:31 +0100

    fuzzel/scripts/fuzzel-files.fish: exclude some directories

--- home/programs/wayland/fuzzel/scripts/fuzzel-files.fish
+++ home/programs/wayland/fuzzel/scripts/fuzzel-files.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-open (fd | fuzzel --dmenu --prompt " " --placeholder "Search for files/directories...")
+open (fd -E Games -E Documents/Codes/nixpkgs | fuzzel --dmenu --prompt " " --placeholder "Search for files/directories...")

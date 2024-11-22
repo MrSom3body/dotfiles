7e98ebe7d9fa40c98b63cd7e47de0f1cce222783
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-22 10:37:08 +0100

    cliphist: make services compatible with uwsm

--- home/services/wayland/cliphist.nix
+++ home/services/wayland/cliphist.nix
@@ -1,6 +1,11 @@
-{
+{lib, ...}: {
   services.cliphist = {
     enable = true;
     allowImages = true;
+  };
+
+  systemd.user.services = {
+    cliphist.Unit.After = lib.mkForce ["graphical-session.target"];
+    cliphist-images.Unit.After = lib.mkForce ["graphical-session.target"];
   };
 }

404806b1e92823495f5b8d365866c793d4b103ab
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-24 19:51:09 +0100

    gns3: don't use the gns3-server service because of https://github.com/NixOS/nixpkgs/issues/343278

--- system/services/gns3.nix
+++ system/services/gns3.nix
@@ -1,13 +1,31 @@
 {pkgs, ...}: {
-  services.gns3-server = {
-    enable = true;
-    dynamips.enable = true;
-    ubridge.enable = true;
-    vpcs.enable = true;
+  # services.gns3-server = {
+  #   enable = true;
+  #   dynamips.enable = true;
+  #   ubridge.enable = true;
+  #   vpcs.enable = true;
+  # };
+
+  # systemd.services.gns3-server.path = [pkgs.vmware-workstation];
+
+  users.groups.ubridge = {};
+  users.users.karun.extraGroups = ["ubridge"];
+
+  security.wrappers.ubridge = {
+    source = "/run/current-system/sw/bin/ubridge";
+    capabilities = "cap_net_admin,cap_net_raw=ep";
+    owner = "root";
+    group = "ubridge";
+    permissions = "u+rx,g+rx,o+rx";
   };
 
   environment.systemPackages = with pkgs; [
     gns3-gui
+    gns3-server
+
+    dynamips
     inetutils
+    ubridge
+    vpcs
   ];
 }

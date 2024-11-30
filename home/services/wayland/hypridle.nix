f9a415e7d2e6064779f67ac39c8b5600aff4f6b7
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-30 20:17:22 +0100

    hypridle: start hyprlock with uwsm

--- home/services/wayland/hypridle.nix
+++ home/services/wayland/hypridle.nix
@@ -12,11 +12,12 @@
     settings = {
       general = {
         ignore_dbus_inhibit = false;
-        lock_cmd = "pidof hyprlock || hyprlock";
+        lock_cmd = "uwsm app -- hyprlock";
         unlock_cmd = "pkill -SIGUSR1 hyprlock";
         before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
+
       listener = [
         {
           timeout = 30;

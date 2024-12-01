80d42c28264bf287499984a3d2ad43992a11efce
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-01 13:27:54 +0100

    fprintd: remove hyprlock pam file

--- system/services/fprintd.nix
+++ system/services/fprintd.nix
@@ -6,14 +6,4 @@
       driver = pkgs.libfprint-2-tod1-elan;
     };
   };
-
-  security.pam.services = {
-    hyprlock = {
-      text = ''
-        auth sufficient pam_unix.so try_first_pass likeauth nullok
-        auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
-        auth include login
-      '';
-    };
-  };
 }

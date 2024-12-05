0f9dc6b257f43a9661ce66f7256d5df708ec4e44
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-05 08:41:18 +0100

    quickshell: make it work with qmlls

--- home/programs/wayland/quickshell/default.nix
+++ home/programs/wayland/quickshell/default.nix
@@ -3,7 +3,21 @@
   pkgs,
   ...
 }: {
-  home.packages = [
-    inputs.quickshell.packages.${pkgs.system}.default
-  ];
+  home = {
+    packages = [
+      inputs.quickshell.packages.${pkgs.system}.default
+    ];
+
+    file.".config/quickshell/.qmlls.ini".text =
+      # ini
+      ''
+        [General]
+        buildDir=${inputs.quickshell.packages.${pkgs.system}.default}/lib/qt-6/qml
+        no-cmake-calls=true
+      '';
+
+    # sessionVariables = {
+    #   QML2_IMPORT_PATH = "${pkgs.kdePackages.qtdeclarative}/lib/qt-6/qml:${inputs.quickshell.packages.${pkgs.system}.default}/lib/qt-6/qml";
+    # };
+  };
 }

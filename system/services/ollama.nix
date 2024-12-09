98d01c4dbd016210e90a73b6db14cf45d9f23f3a
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-09 20:29:37 +0100

    ollama: only enable in specialization

--- system/services/ollama.nix
+++ system/services/ollama.nix
@@ -1,18 +1,12 @@
 {
-  config,
-  lib,
-  ...
-}: {
-  services.ollama = {
-    enable = true;
-    loadModels = ["llama3.2:3b-instruct-q4_K_M" "codellama:7b-code-q8_0" "llava"];
-    acceleration = "cuda";
-  };
+  specialisation.enableOllama.configuration = {
+    environment.etc."specialisation".text = "enableOllama";
+    services.ollama = {
+      enable = true;
+      loadModels = ["llama3.2:3b-instruct-q4_K_M" "codellama:7b-code-q8_0" "llava"];
+      acceleration = "cuda";
+    };
 
-  boot.kernelModules = lib.mkIf config.services.ollama.enable ["nvidia_uvm"];
-
-  specialisation.ollamaNoGPU.configuration = {
-    environment.etc."specialisation".text = "ollamaNoGPU";
-    services.ollama.acceleration = lib.mkForce false;
+    boot.kernelModules = ["nvidia_uvm"];
   };
 }

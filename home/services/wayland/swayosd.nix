0f6d492ec0b1543f2af918030476fa66d91dab3b
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-25 22:13:32 +0100

    swayosd: make it fit in better to the current theme (border, opacity, etc.)

--- home/services/wayland/swayosd.nix
+++ home/services/wayland/swayosd.nix
@@ -1,6 +1,40 @@
 {
+  config,
+  dotfiles,
+  ...
+}: {
   services.swayosd = {
     enable = true;
     topMargin = 0.5;
   };
+
+  home.file.".config/swayosd/style.css".text = with config.lib.stylix.colors.withHashtag; let
+    inherit (dotfiles.appearance) border;
+  in
+    # css
+    ''
+      window {
+        border-radius: ${toString border.radius};
+        border: solid ${base0D} ${toString border.size};
+        background: alpha(${base00}, ${toString config.stylix.opacity.popups});
+      }
+
+      image,
+      label {
+        color: ${base05};
+      }
+
+      progressbar:disabled,
+      image:disabled {
+        opacity: 0.5;
+      }
+
+      trough {
+        background: alpha(${base05}, 0.5);
+      }
+
+      progress {
+        background: ${base05};
+      }
+    '';
 }

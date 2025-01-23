9d6bce025b9d7e053c1cb8040c305b0337bb3c08
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-23 20:46:30 +0100

    pkgs: sort alphabetically

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  wl-ocr = pkgs.callPackage ./wl-ocr {};
-  hyprcast = pkgs.callPackage ./hyprcast {};
   fuzzel-goodies = pkgs.callPackage ./fuzzel-goodies {};
+  hyprcast = pkgs.callPackage ./hyprcast {};
+  wl-ocr = pkgs.callPackage ./wl-ocr {};
 }

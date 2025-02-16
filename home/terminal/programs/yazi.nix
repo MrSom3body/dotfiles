63e5c97b16b8f442596d8de21bab7f2b7894c811
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-16 16:40:33 +0100

    yazi: update plugins

--- home/terminal/programs/yazi.nix
+++ home/terminal/programs/yazi.nix
@@ -2,8 +2,8 @@
   plugins-repo = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "ec97f8847feeb0307d240e7dc0f11d2d41ebd99d";
-    sha256 = "sha256-By8XuqVJvS841u+8Dfm6R8GqRAs0mO2WapK6r2g7WI8=";
+    rev = "beb586aed0d41e6fdec5bba7816337fdad905a33";
+    sha256 = "sha256-enIt79UvQnKJalBtzSEdUkjNHjNJuKUWC4L6QFb3Ou4=";
   };
 in {
   home.packages = with pkgs; [ripdrag];
@@ -30,8 +30,8 @@ in {
       starship = pkgs.fetchFromGitHub {
         owner = "Rolv-Apneseth";
         repo = "starship.yazi";
-        rev = "247f49da1c408235202848c0897289ed51b69343";
-        sha256 = "sha256-0J6hxcdDX9b63adVlNVWysRR5htwAtP5WhIJ2AK2+Gs=";
+        rev = "f6939fbdbc3fdfcdc2a80251841e429e0cd5cf3c";
+        sha256 = "sha256-5QQsFozbulgLY/Gl6QuKSOTtygULveoRD49V00e0WOw=";
       };
     };
 
@@ -48,10 +48,7 @@ in {
           desc = "Maximize or restore the preview pane";
         }
         {
-          on = [
-            "c"
-            "m"
-          ];
+          on = ["c" "m"];
           run = "plugin chmod";
           desc = "Chmod on selected files";
         }

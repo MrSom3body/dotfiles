4f4f480737c917b7143340e0233e59851b011fc9
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-17 13:20:02 +0100

    vesktop: use older electron 33 so that streaming works

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [vesktop];
+  home.packages = with pkgs; [
+    # TODO revert electron version when streaming works again
+    (vesktop.override {
+      electron = pkgs.electron_33;
+    })
+  ];
+
   home.file.".config/wireplumber/main.lua.d/99-stop-vesktop-microphone-adjust.lua".text = ''
     table.insert(default_access.rules, {
         matches = {

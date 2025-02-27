48b9931ae144f168f3559f37f1b3d49a3fbf4ef2
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-27 21:30:47 +0100

    vesktop: disable overriding electron version

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -1,9 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # TODO revert electron version when streaming works again
-    (vesktop.override {
-      electron = pkgs.electron_33;
-    })
+    vesktop
   ];
 
   home.file.".config/wireplumber/main.lua.d/99-stop-vesktop-microphone-adjust.lua".text = ''

86557561ea0a4630cf3dd43268d49aacfc76a58f
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-01 15:41:08 +0100

    overlays: add pandoc to obsidian

---
+++ overlays/obsidian/default.nix
@@ -0,0 +1,12 @@
+(
+  final: prev: {
+    obsidian = prev.obsidian.overrideAttrs (oldAttrs: {
+      postInstall =
+        (oldAttrs.postInstall or "")
+        + ''
+          wrapProgram $out/bin/obsidian \
+            --prefix PATH : ${final.lib.makeBinPath [final.pandoc]}
+        '';
+    });
+  }
+)

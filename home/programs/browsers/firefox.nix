d8f95434de92f41a224a8d584e39509325a8708e
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-09 11:19:39 +0100

    firefox: some stuff - policies do not work -> remove them - switch to bin package - move about:config prefs to profile

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -12,8 +12,8 @@ in {
     enable = true;
     package =
       if settings.programs.browser == "firefox-beta"
-      then pkgs.firefox-beta
-      else pkgs.firefox;
+      then pkgs.firefox-beta-bin
+      else pkgs.firefox-bin;
     profiles.default = {
       id = 0;
       name = "default";
@@ -22,40 +22,40 @@ in {
         force = true;
         default = "DuckDuckGo";
         engines = {
-          "Startpage" = {
-            urls = [{template = "https://www.startpage.com/do/search?query={searchTerms}";}];
-            iconUpdateURL = "https://www.startpage.com/favicon.ico";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["!sp"];
-          };
           "ProtonDB" = {
             urls = [{template = "https://protondb.com/search?q={searchTerms}";}];
             icon = "https://protondb.com/favicon.ico";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["!pdb"];
+            definedAliases = ["@pdb"];
           };
           "GitHub" = {
             urls = [{template = "https://github.com/search?q={searchTerms}";}];
             iconUpdateURL = "https://github.com/favicon.ico";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["!gh"];
+            definedAliases = ["@gh"];
           };
           "Alternativeto.net" = {
             urls = [{template = "https://alternativeto.net/browse/search/?q={searchTerms}";}];
             iconUpdateURL = "https://alternativeto.net/favicon.ico";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["!alt"];
+            definedAliases = ["@alt"];
           };
           "YouTube" = {
             urls = [{template = "https://www.youtube.com/results?search_query={searchTerms}";}];
             iconUpdateURL = "https://www.youtube.com/favicon.ico";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["!yt"];
+            definedAliases = ["@yt"];
+          };
+          "NixOS Wiki" = {
+            urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
+            iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@nw"];
           };
           "MyNixOS" = {
             urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["!no"];
+            definedAliases = ["@no"];
           };
           "Nix Packages" = {
             urls = [
@@ -78,52 +78,12 @@ in {
               }
             ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["!np"];
+            definedAliases = ["@np"];
           };
         };
       };
-    };
 
-    policies = {
-      AutofillCreditCardEnabled = false;
-      DisablePocket = true;
-      DisplayBookmarksToolbar = "newtab";
-      DisplayMenuBar = "default-off";
-      DontCheckDefaultBrowser = true;
-      EnableTrackingProtection = {
-        Value = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-        EmailTracking = true;
-      };
-      FirefoxHome = {
-        Search = true;
-        TopSites = true;
-        Highlights = true;
-        Pocket = false;
-        Snippets = true;
-      };
-      FirefoxSuggest = {
-        WebSuggestions = false;
-        SponsoredSuggestions = false;
-        ImproveSuggest = true;
-        Locked = true;
-      };
-      HardwareAcceleration = true;
-      Homepage = {
-        StartPage = "homepage";
-      };
-      HttpsOnlyMode = "force_enabled";
-      NewTabPage = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      PasswordManagerEnabled = false;
-      SearchBar = "unified";
-      SearchSuggestEnabled = true;
-      TranslateEnabled = true;
-
-      # about:config
-      Preferences = {
+      settings = {
         "browser.ctrlTab.sortByRecentlyUsed" = lock-true;
         "browser.tabs.hoverPreview.enabled" = lock-true;
         "browser.tabs.inTitlebar" = {
@@ -131,8 +91,6 @@ in {
           Status = "locked";
         };
         "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = lock-true;
-        "sidebar.revamp" = lock-true;
-        "sidebar.verticalTabs" = lock-true;
       };
     };
   };

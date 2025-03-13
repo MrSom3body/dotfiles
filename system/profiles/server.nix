ddac03bb221bbeea1a92e28d38c846be98ec0a36
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-13 20:15:10 +0100

    system/profiles: add server profile

---
+++ system/profiles/server.nix
@@ -0,0 +1,22 @@
+{inputs, ...}: {
+  imports = [
+    ../../system/core
+
+    ../../system/hardware/fwupd.nix
+
+    ../../system/network
+    ../../system/network/avahi.nix
+
+    ../../system/programs
+    ../../system/programs/command-not-found.nix
+
+    ../../system/services
+
+    ../../system/virtualisation/vm.nix
+
+    ../../system/style/stylix.nix
+    inputs.stylix.nixosModules.stylix
+  ];
+
+  hardware.enableAllFirmware = true;
+}
--- system/profiles/workstation.nix
+++ system/profiles/workstation.nix
@@ -1,30 +1,15 @@
-{inputs, ...}: {
+{...}: {
   imports = [
-    ../../system/core
+    ./server.nix
 
-    ../../system/hardware/fwupd.nix
     ../../system/hardware/graphics.nix
 
-    ../../system/network
-    ../../system/network/avahi.nix
-
-    ../../system/programs
     ../../system/programs/hyprland.nix
-    ../../system/programs/command-not-found.nix
 
-    ../../system/services
     ../../system/services/flatpak.nix
     ../../system/services/gnome-services.nix
     ../../system/services/greetd.nix
     ../../system/services/pipewire.nix
     ../../system/services/printing.nix
-
-    ../../system/virtualisation/vm.nix
-
-    ../../system/style/stylix.nix
-
-    inputs.stylix.nixosModules.stylix
   ];
-
-  hardware.enableAllFirmware = true;
 }

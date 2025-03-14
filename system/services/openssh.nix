2948dea9ef795293e4a08227f4945a08716f3a24
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-14 10:57:38 +0100

    openssh: don't allow password login

--- system/services/openssh.nix
+++ system/services/openssh.nix
@@ -1,7 +1,11 @@
 {settings, ...}: {
   services.openssh = {
     enable = true;
-    settings.UseDns = true;
+    settings = {
+      UseDns = true;
+      PasswordAuthentication = false;
+      ChallengeResponseAuthentication = false;
+    };
   };
 
   users.users.${settings.user}.openssh.authorizedKeys.keys = settings.authorizedSshKeys;

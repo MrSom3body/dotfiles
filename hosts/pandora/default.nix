9a612e2ba94b74edd530dcc2c508a94e1d33aa19
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-17 17:22:50 +0100

    hosts/pandora: move minecraft service into it's own file

--- hosts/pandora/default.nix
+++ hosts/pandora/default.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   imports =
     [
       ../../system/profiles/server.nix
@@ -23,23 +19,6 @@
     ];
 
   services = {
-    minecraft-servers.servers = {
-      test = {
-        enable = false;
-        enableReload = true;
-        openFirewall = true;
-        package = pkgs.vanillaServers.vanilla-1_21_1;
-        serverProperties = {
-          difficulty = "normal";
-          enforce-secure-profile = false;
-          motd = "Server managed my pandora";
-          online-mode = false;
-          server-port = 25565;
-        };
-        jvmOpts = "-Xms1G -Xmx4G";
-      };
-    };
-
     tailscale = {
       useRoutingFeatures = "server";
       extraUpFlags = [

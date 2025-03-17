a414ff7e16e3189450da444243bb198eb1be249f
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-17 16:19:23 +0100

    hosts/pandora/services/send.nix: lower options

--- hosts/pandora/services/send.nix
+++ hosts/pandora/services/send.nix
@@ -7,6 +7,14 @@ in {
       host = "127.0.0.1";
       port = 1443;
       openFirewall = true;
+      environment = {
+        MAX_EXPIRE_SECONDS = 345600;
+        MAX_DOWNLOADS = 10;
+        DOWNLOAD_COUNTS = "1,2,3,5,10";
+        EXPIRE_TIMES_SECONDS = "300,3600,86400,259200";
+        DEFAULT_DOWNLOADS = 1;
+        DEFAULT_EXPIRE_SECONDS = 86400;
+      };
     };
 
     caddy.virtualHosts."send.sndh.dev" = {

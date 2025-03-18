f7f7a6a8fae5def4e505afe1c07159eb38698bcf
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-18 21:26:50 +0100

    system/services/tailscale.nix: move network-dispatcher config to pandora

--- hosts/pandora/default.nix
+++ hosts/pandora/default.nix
@@ -1,4 +1,10 @@
-{inputs, ...}: {
+{
+  lib,
+  inputs,
+  pkgs,
+  config,
+  ...
+}: {
   imports =
     [
       ../../system/profiles/server.nix
@@ -19,12 +25,24 @@
       (inputs.nixos-hardware + "/common/cpu/intel/haswell")
     ];
 
-  services.tailscale = {
-    useRoutingFeatures = "server";
-    extraUpFlags = [
-      "--advertise-exit-node"
-      "--advertise-routes \"10.0.0.10/32\""
-    ];
+  services = {
+    tailscale = {
+      useRoutingFeatures = "server";
+      extraUpFlags = [
+        "--advertise-exit-node"
+        "--advertise-routes \"10.0.0.10/32\""
+      ];
+    };
+
+    networkd-dispatcher = lib.optionalAttrs (config.services.tailscale.useRoutingFeatures == "server") {
+      enable = true;
+      rules."50-tailscale" = {
+        onState = ["routable"];
+        script = ''
+          ${lib.getExe pkgs.ethtool} -K eno1 rx-udp-gro-forwarding on rx-gro-list off
+        '';
+      };
+    };
   };
 
   security.tpm2.enable = true;
--- system/services/tailscale.nix
+++ system/services/tailscale.nix
@@ -1,22 +1,7 @@
 {
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
   services = {
     tailscale = {
       enable = true;
-    };
-
-    networkd-dispatcher = lib.optionalAttrs (config.services.tailscale.useRoutingFeatures == "server") {
-      enable = true;
-      rules."50-tailscale" = {
-        onState = ["routable"];
-        script = ''
-          ${lib.getExe pkgs.ethtool} -K eno1 rx-udp-gro-forwarding on rx-gro-list off
-        '';
-      };
     };
   };
 }

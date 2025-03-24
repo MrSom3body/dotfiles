e1a7c9150c89b643b1a4ff34db80a4c091ea099c
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-24 09:17:22 +0100

    flake.lock: update

--- system/services/caddy.nix
+++ system/services/caddy.nix
@@ -3,7 +3,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de"];
-      hash = "sha256-3nvVGW+ZHLxQxc1VCc/oTzCLZPBKgw4mhn+O3IoyiSs=";
+      hash = "sha256-YYpsf8HMONR1teMiSymo2y+HrKoxuJMKIea5/NEykGc=";
     };
   };
 }

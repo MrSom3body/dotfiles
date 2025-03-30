2012bbe0ddb95c044898d50fbcc6ed2d68634514
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-30 10:53:23 +0200

    system/services/send: allow more downloads

--- system/services/send.nix
+++ system/services/send.nix
@@ -7,7 +7,7 @@
       environment = {
         MAX_EXPIRE_SECONDS = 345600;
         MAX_DOWNLOADS = 10;
-        DOWNLOAD_COUNTS = "1,2,3,5,10";
+        DOWNLOAD_COUNTS = "1,2,3,5,10,25,50";
         EXPIRE_TIMES_SECONDS = "300,3600,86400,259200";
         DEFAULT_DOWNLOADS = 1;
         DEFAULT_EXPIRE_SECONDS = 86400;

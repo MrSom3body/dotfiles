dc39d4be90b88ec61f4e9009e855b5c49e4417c0
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-30 13:59:09 +0200

    xdg: mimeApps rework

--- home/terminal/programs/xdg.nix
+++ home/terminal/programs/xdg.nix
@@ -3,23 +3,60 @@
   pkgs,
   settings,
   ...
-}: {
+}: let
+  browser = ["firefox.desktop"];
+  imageViewer = ["org.gnome.Loupe.desktop"];
+  videoPlayer = ["mpv.desktop"];
+  audioPlayer = ["io.bassi.Amberol.desktop"];
+
+  xdgAssociations = type: list: program:
+    builtins.listToAttrs (map (e: {
+        name = "${type}/${e}";
+        value = program;
+      })
+      list);
+
+  images = xdgAssociations "image" ["png" "svg" "jpeg" "gif"] imageViewer;
+  audios = xdgAssociations "audio" ["mp3" "flac" "wav" "aac"] audioPlayer;
+  videos = xdgAssociations "video" ["mp4" "avi" "mkv"] videoPlayer;
+
+  browserTypes =
+    (xdgAssociations "application" [
+        "json"
+        "x-extension-htm"
+        "x-extension-html"
+        "x-extension-shtml"
+        "x-extension-xht"
+        "x-extension-xhtml"
+        "xhtml+xml"
+      ]
+      browser)
+    // (xdgAssociations "x-scheme-handler" [
+        "about"
+        "chrome"
+        "ftp"
+        "http"
+        "https"
+        "mailto"
+        "unknown"
+      ]
+      browser);
+in {
   xdg = {
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "application/pdf" = ["org.gnome.Papers.desktop"]; # PDFs
-        "application/zip" = ["org.gnome.Nautilus.desktop" "org.gnome.FileRoller.desktop"]; # ZIPs
-        "image/*" = ["org.gnome.Loupe.desktop"]; # Images
-        "image/jpeg" = ["org.gnome.Loupe.desktop"]; # Images
-        "image/jpg" = ["org.gnome.Loupe.desktop"]; # Images
-        "image/png" = ["org.gnome.Loupe.desktop"]; # Images
-        "inode/directory" = ["yazi.desktop"]; # Files
-        "text/plain" = ["Helix.desktop"]; # Plain text
-        "x-scheme-handler/http" = ["firefox.desktop"]; # Links
-        "x-scheme-handler/https" = ["firefox.desktop"]; # Links
-        "x-scheme-handler/mailto" = ["firefox.desktop"]; # Links
-      };
+      defaultApplications =
+        {
+          "application/pdf" = ["org.gnome.Papers.desktop"]; # PDFs
+          "application/zip" = ["org.gnome.Nautilus.desktop" "org.gnome.FileRoller.desktop"]; # ZIPs
+          "inode/directory" = ["yazi.desktop"]; # Files
+          "text/html" = browser; # HTML files
+          "text/plain" = ["Helix.desktop"]; # Plain text
+        }
+        // images
+        // audios
+        // videos
+        // browserTypes;
     };
 
     userDirs = {

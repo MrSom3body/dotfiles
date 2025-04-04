a4ccafa2cfc4dd7b101a9ccc6d0bad9a06ca71e6
karun <129101708+MrSom3body@users.noreply.github.com>
2025-04-04 10:34:25 +0200

    transmission: seed until seed ratio reaches 1

--- system/services/transmission.nix
+++ system/services/transmission.nix
@@ -5,10 +5,7 @@
       speed-limit-up = 500;
       speed-limit-up-enabled = true;
 
-      idle-seeding-limit = 180;
-      idle-seeding-limit-enabled = true;
-
-      ratio-limit = 2;
+      ratio-limit = 1;
       ratio-limit-enabled = true;
     };
   };

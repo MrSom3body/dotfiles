a195b880782487c52f9c809bc43997dcb075549c
karun <129101708+MrSom3body@users.noreply.github.com>
2025-04-04 21:34:58 +0200

    swayosd: fix style.css finally

--- home/services/wayland/swayosd.nix
+++ home/services/wayland/swayosd.nix
@@ -1,40 +1,45 @@
 {
   config,
+  pkgs,
   settings,
   ...
 }: {
   services.swayosd = {
     enable = true;
     topMargin = 0.5;
+    stylePath = with config.lib.stylix.colors.withHashtag; let
+      inherit (settings.appearance) border;
+    in
+      pkgs.writeText "style.css" # css
+      
+      ''
+        window#osd {
+          border-radius: ${toString border.radius}px;
+          border: solid ${base0D} ${toString border.size}px;
+          background: alpha(${base00}, ${toString config.stylix.opacity.popups});
+        }
+
+        #container {
+          margin: 8px;
+        }
+
+        window#osd image,
+        window#osd label {
+          color: ${base05};
+        }
+
+        window#osd progressbar:disabled,
+        window#osd image:disabled {
+          opacity: 0.5;
+        }
+
+        window#osd trough {
+          background: alpha(${base05}, 0.5);
+        }
+
+        window#osd progress {
+          background: ${base05};
+        }
+      '';
   };
-
-  home.file.".config/swayosd/style.css".text = with config.lib.stylix.colors.withHashtag; let
-    inherit (settings.appearance) border;
-  in
-    # css
-    ''
-      window {
-        border-radius: ${toString border.radius};
-        border: solid ${base0D} ${toString border.size};
-        background: alpha(${base00}, ${toString config.stylix.opacity.popups});
-      }
-
-      image,
-      label {
-        color: ${base05};
-      }
-
-      progressbar:disabled,
-      image:disabled {
-        opacity: 0.5;
-      }
-
-      trough {
-        background: alpha(${base05}, 0.5);
-      }
-
-      progress {
-        background: ${base05};
-      }
-    '';
 }

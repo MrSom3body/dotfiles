f02995e1cf94b680224d587c80226ff98690f967
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-06 22:13:25 +0200

    jellyfin: open port

--- system/services/jellyfin.nix
+++ system/services/jellyfin.nix
@@ -4,4 +4,15 @@
   };
 
   users.users.jellyfin.extraGroups = ["video" "render"];
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      8096 # http traffic
+    ];
+
+    allowedUDPPorts = [
+      1900 # service discovery
+      7359 # client discovery
+    ];
+  };
 }

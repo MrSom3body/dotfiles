60b4aaef8f442b825c561f88da25d32c20ed00f4
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-06 14:54:18 +0200

    nix/substituters: remove helix

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -4,14 +4,12 @@
       "https://cache.nixos.org?priority=10"
 
       "https://cuda-maintainers.cachix.org"
-      "https://helix.cachix.org"
       "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };

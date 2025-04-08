493a2d2f7ec8e3b9c02ca32e96b9d9067c78b95e
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-08 13:32:27 +0200

    proton: add more output for transmission port forwarding

--- hosts/pandora/vpn/proton.nix
+++ hosts/pandora/vpn/proton.nix
@@ -28,7 +28,7 @@
         pkgs.writeShellScript "protonvpn-port-forwarding" ''
           while true; do
               date
-              natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > ${outPath} || {
+              natpmpc -a 1 0 udp 60 -g 10.2.0.1 > ${outPath} && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 >> ${outPath} || {
                   echo -e "ERROR with natpmpc command \a"
                   break
               }

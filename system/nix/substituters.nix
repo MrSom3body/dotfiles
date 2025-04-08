f2ccaed3e9ea7841779f90912c43545235a4c5be
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-08 11:58:02 +0200

    nix/substituters: add nix-community cache

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -2,12 +2,14 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org?priority=10"
+      "https://nix-community.cachix.org"
 
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
 
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="

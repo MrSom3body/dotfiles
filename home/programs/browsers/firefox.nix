23aa0345090bd6584e0031622c24e5cfe28bb97e
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-10 10:48:29 +0200

    firefox: enable tab groups

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -16,6 +16,8 @@
       isDefault = true;
       settings = {
         "browser.download.start_downloads_in_tmp_dir" = true;
+        "browser.tabs.groups.enabled" = true;
+        "browser.tabs.groups.smart.enabled" = true;
         "cookiebanners.service.mode" = 2;
         "cookiebanners.service.mode.privateBrowsing" = 2;
         "cookiebanners.ui.desktop.enabled" = 2;

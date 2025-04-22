c7129410f350e90ef31ef5c583362ab17fc21c86
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-22 22:15:55 +0200

    ollama: add deepseek-rs:14b and remove codellama:7b-code-q8_0

--- hosts/common/optional/services/ollama.nix
+++ hosts/common/optional/services/ollama.nix
@@ -3,7 +3,11 @@
     environment.etc."specialisation".text = "enableOllama";
     services.ollama = {
       enable = true;
-      loadModels = ["llama3.2:3b-instruct-q4_K_M" "codellama:7b-code-q8_0" "llava"];
+      loadModels = [
+        "deepseek-r1:14b"
+        "llama3.2:3b-instruct-q4_K_M"
+        "llava"
+      ];
     };
   };
 }

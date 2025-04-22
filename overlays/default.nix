d1bfa73d0d3165c8e89b45994cb99c7656eb4d13
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-22 11:27:04 +0200

    flake.lock: update overlays/modifications: add overlay for jetbrains (https://github.com/NixOS/nixpkgs/issues/400317)

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,7 +9,16 @@
       inherit (final) pkgs;
     };
 
-  modifications = final: prev: {
+  modifications = final: prev: let
+    nixpkgs-jetbrains =
+      import (builtins.fetchTarball {
+        url = "https://github.com/nixos/nixpkgs/archive/42a1c966be226125b48c384171c44c651c236c22.tar.gz";
+        sha256 = "sha256:082dpl311xlspwm5l3h2hf10ww6l59m7k2g2hdrqs4kwwsj9x6mf";
+      }) {
+        inherit (final.pkgs) system;
+        config.allowUnfree = true;
+      };
+  in {
     bemoji = prev.bemoji.overrideAttrs (_oldAttrs: {
       version = "unstable-2025-03-17";
       src = final.fetchFromGitHub {
@@ -56,6 +65,9 @@
       .override {
         nodejs = final.nodejs_18;
       };
+
+    # TODO delete when https://github.com/NixOS/nixpkgs/issues/400317 gets resolved
+    inherit (nixpkgs-jetbrains) jetbrains;
   };
 
   stable-packages = final: _prev: {

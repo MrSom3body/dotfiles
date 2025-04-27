a279795a35651b50dedb665e4fa42d48dad0a9d8
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-27 17:15:51 +0200

    vesktop: use home-manager options

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -1,7 +1,14 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    vesktop
-  ];
+{
+  programs.vesktop = {
+    enable = true;
+    settings = {
+      discordBranch = "canary";
+      minimizeToTray = false;
+      arRPC = true;
+      splashTheming = true;
+      enableMenu = true;
+    };
+  };
 
   home.file.".config/wireplumber/main.lua.d/99-stop-vesktop-microphone-adjust.lua".text = ''
     table.insert(default_access.rules, {

28f8c05bd3d8fb00824d66be6063299b23bc3586
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-04-29 12:21:02 +0200

    vesktop: remove wireplumber config to disable microphone gain (does not work)

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -9,15 +9,4 @@
       enableMenu = true;
     };
   };
-
-  home.file.".config/wireplumber/main.lua.d/99-stop-vesktop-microphone-adjust.lua".text = ''
-    table.insert(default_access.rules, {
-        matches = {
-            {
-                { "application.process.binary", "=", "vesktop.bin" },
-            },
-        },
-        default_permissions = "rx",
-    })
-  '';
 }

eac4098c4e560b8734d3973cdc927fbbf174fc3c
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-05-04 10:11:29 +0200

    flake.lock: update glance: remove theme (stylix does it now)

--- hosts/common/optional/services/glance.nix
+++ hosts/common/optional/services/glance.nix
@@ -1,55 +1,10 @@
 {
-  config,
-  lib,
-  ...
-}: {
   services.glance = {
     enable = true;
     settings = {
       server = {
         host = "127.0.0.1";
         port = 8080;
-      };
-
-      # copied from https://github.com/danth/stylix/blob/d9df91c55643a8b5229a3ae3a496a30f14965457/modules/glance/hm.nix
-      theme = let
-        rgb-to-hsl = color: let
-          r = ((lib.toInt config.lib.stylix.colors."${color}-rgb-r") * 100.0) / 255;
-          g = ((lib.toInt config.lib.stylix.colors."${color}-rgb-g") * 100.0) / 255;
-          b = ((lib.toInt config.lib.stylix.colors."${color}-rgb-b") * 100.0) / 255;
-          max = lib.max r (lib.max g b);
-          min = lib.min r (lib.min g b);
-          delta = max - min;
-          fmod = base: int: base - (int * builtins.floor (base / int));
-          h =
-            if delta == 0
-            then 0
-            else if max == r
-            then 60 * (fmod ((g - b) / delta) 6)
-            else if max == g
-            then 60 * (((b - r) / delta) + 2)
-            else if max == b
-            then 60 * (((r - g) / delta) + 4)
-            else 0;
-          l = (max + min) / 2;
-          s =
-            if delta == 0
-            then 0
-            else 100 * delta / (100 - lib.max (2 * l - 100) (100 - (2 * l)));
-          roundToString = value: toString (builtins.floor (value + 0.5));
-        in
-          lib.concatMapStringsSep " " roundToString [
-            h
-            s
-            l
-          ];
-      in {
-        light = config.stylix.polarity == "light";
-        contrast-multiplier = 1.0;
-        background-color = rgb-to-hsl "base00";
-        primary-color = rgb-to-hsl "base05";
-        positive-color = rgb-to-hsl "base0B";
-        negative-color = rgb-to-hsl "base08";
       };
 
       pages = [

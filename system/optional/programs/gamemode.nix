58fcfa9bd1044b6fc4a0399fb4e45f4a7563d81e
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-05-21 08:36:31 +0200

    gamemode: send notifications and set renice to 6

--- system/optional/programs/gamemode.nix
+++ system/optional/programs/gamemode.nix
@@ -1,6 +1,21 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   programs.gamemode = {
     enable = true;
-    settings.general.softrealtime = "auto";
+    settings = {
+      general = {
+        renice = 6;
+        softrealtime = "auto";
+      };
+      custom = let
+        notify = title: "${lib.getExe pkgs.libnotify} -a gamemoded -u critical -t 3000 '${title}'";
+      in {
+        start = notify "Game Mode enabled!";
+        end = notify "Game Mode disabled!";
+      };
+    };
   };
 }

0258d63525331aa93b023cf30d28889d780b6133
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-06-05 23:55:34 +0200

    fnott-dnd: set correct icons

--- home/services/wayland/fnott/scripts/fnott-dnd.fish
+++ home/services/wayland/fnott/scripts/fnott-dnd.fish
@@ -28,12 +28,12 @@ function toggle
     switch $fnott_state
         case pausing
             fnottctl unpause
-            notify-send -a fnott -i notification-active "Do Not Disturb" deactivated
+            notify-send -a fnott -i notifications-symbolic "Do Not Disturb" deactivated
         case unpausing
-            notify-send -a fnott -i notification-disabled "Do Not Disturb" activated
+            notify-send -a fnott -i notifications-disabled-symbolic "Do Not Disturb" activated
             fnottctl pause
         case '*'
-            notify-send -a fnott -i notification-disabled "Do Not Disturb" activated
+            notify-send -a fnott -i notifications-disabled-symbolic "Do Not Disturb" activated
             fnottctl pause
     end
     pkill -36 waybar

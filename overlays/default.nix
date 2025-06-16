869cd6f047179094d8a3ffe1a3330fec49f2e5f6
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-06-16 23:04:09 +0200

    overlays: remove vpnc

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,16 +38,6 @@
         '';
     });
 
-    vpnc = prev.vpnc.overrideAttrs (_oldAttrs: {
-      version = "unstable-2024-12-20";
-      src = final.fetchFromGitHub {
-        owner = "streambinder";
-        repo = "vpnc";
-        rev = "d58afaaafb6a43cb21bb08282b54480d7b2cc6ab";
-        hash = "sha256-79DaK1s+YmROKbcWIXte+GZh0qq9LAQlSmczooR86H8=";
-      };
-    });
-
     # TODO delete when https://github.com/NixOS/nixpkgs/pull/411885 gets merged
     linuxPackages_latest = prev.linuxPackages_latest.extend (_lpfinal: _lpprev: {
       vmware = prev.linuxPackages_latest.vmware.overrideAttrs (oldAttrs: {

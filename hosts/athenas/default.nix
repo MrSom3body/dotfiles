beea4678045e835406b5d6cd75cb5b70f493be53
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-07-08 00:03:46 +0200

    hosts/athenas: enable home-manager

--- hosts/athenas/default.nix
+++ hosts/athenas/default.nix
@@ -5,6 +5,8 @@
     ../../system/profiles/laptop.nix
   ];
 
+  my.home-manager.enable = true;
+
   boot.supportedFilesystems = ["ntfs"]; # allow mounting windows
 
   nixpkgs.hostPlatform = "x86_64-linux";

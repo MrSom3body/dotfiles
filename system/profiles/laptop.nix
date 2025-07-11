75cea0391e69c94a3238181d3e48b4e3b0cbd474
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-07-11 13:11:54 +0200

    system/optional/hardware: add power save module system/profiles/laptop: import power save module

--- system/profiles/laptop.nix
+++ system/profiles/laptop.nix
@@ -5,6 +5,7 @@
     ../optional/network/wifi.nix
 
     ../optional/hardware/bluetooth.nix
+    ../optional/hardware/powersave.nix
 
     ../optional/services/location.nix
     ../optional/services/power.nix

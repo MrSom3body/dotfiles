949a8a41e9e9376c93e8b12758ee01ff61e9120e
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-07-15 23:59:13 +0200

    flake.lock: update overlays: add overlay for jdk8 caddy: update plugins hash

--- system/optional/services/caddy.nix
+++ system/optional/services/caddy.nix
@@ -21,7 +21,7 @@
       '';
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-        hash = "sha256-Gsuo+ripJSgKSYOM9/yl6Kt/6BFCA6BuTDvPdteinAI=";
+        hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
       };
     };
   };

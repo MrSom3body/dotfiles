aa6ac179d72e596f7a06a55791815f937529acd6
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-07-26 15:33:23 +0200

    browsers: split out common settings into a seperate file

--- home/browsers/firefox.nix
+++ home/browsers/firefox.nix
@@ -13,6 +13,8 @@ let
   inherit (lib) mkEnableOption;
   inherit (lib) mkOption;
   cfg = config.my.browsers.firefox;
+
+  defaults = import ./firefoxDefaults.nix { inherit pkgs; };
 in
 {
   options.my.browsers.firefox = {
@@ -43,92 +45,11 @@ in
         id = 0;
         name = "default";
         isDefault = true;
-        settings = {
-          "browser.download.start_downloads_in_tmp_dir" = true;
-          "browser.ml.linkPreview.enabled" = true;
-          "browser.tabs.groups.enabled" = true;
-          "browser.tabs.groups.smart.enabled" = true;
-          "cookiebanners.service.mode" = 2;
-          "cookiebanners.service.mode.privateBrowsing" = 2;
-          "cookiebanners.ui.desktop.enabled" = 2;
-          "media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled" = true;
+        settings = defaults.settings // {
           "sidebar.verticalTabs" = true;
         };
 
-        search = {
-          force = true;
-          default = "searxng";
-          engines = {
-            "searxng" = {
-              urls = [ { template = "https://search.sndh.dev/search?q={searchTerms}"; } ];
-              icon = "https://search.sndh.dev/favicon.ico";
-              definedAliases = [ "@sx" ];
-            };
-            "amazon" = {
-              urls = [ { template = "https://amazon.de/s?k={searchTerms}"; } ];
-              icon = "https://amazon.de/favicon.ico";
-              definedAliases = [ "@a" ];
-            };
-            "protondb" = {
-              urls = [ { template = "https://protondb.com/search?q={searchTerms}"; } ];
-              icon = "https://protondb.com/favicon.ico";
-              definedAliases = [ "@pdb" ];
-            };
-            "github" = {
-              urls = [ { template = "https://github.com/search?q={searchTerms}"; } ];
-              icon = "https://github.com/favicon.ico";
-              definedAliases = [ "@gh" ];
-            };
-            "alternativeto" = {
-              urls = [ { template = "https://alternativeto.net/browse/search/?q={searchTerms}"; } ];
-              icon = "https://alternativeto.net/favicon.ico";
-              definedAliases = [ "@alt" ];
-            };
-            "youtube" = {
-              urls = [ { template = "https://www.youtube.com/results?search_query={searchTerms}"; } ];
-              icon = "https://www.youtube.com/favicon.ico";
-              definedAliases = [ "@yt" ];
-            };
-            "nixos-wiki" = {
-              urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
-              icon = "https://wiki.nixos.org/favicon.png";
-              definedAliases = [ "@nw" ];
-            };
-            "mynixos" = {
-              urls = [ { template = "https://mynixos.com/search?q={searchTerms}"; } ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@no" ];
-            };
-            "nix-packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "channel";
-                      value = "unstable";
-                    }
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-            "subreddit" = {
-              urls = [ { template = "https://reddit.com/r/{searchTerms}"; } ];
-              icon = "https://reddit.com/favicon.png";
-              definedAliases = [ "r/" ];
-            };
-          };
-        };
+        inherit (defaults) search;
       };
     };
   };

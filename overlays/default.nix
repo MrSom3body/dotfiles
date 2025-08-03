129a4b13cc70bbdb60a390f6b1d3bf97c44bd69f
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-03 23:56:28 +0200

    overlays: add vmware-modules

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,6 +22,21 @@
         '';
       });
 
+      # TODO delete when https://github.com/NixOS/nixpkgs/issues/430396 gets resolved
+      linuxPackages_latest = prev.linuxPackages_latest.extend (
+        _lpfinal: _lpprev: {
+          vmware = prev.linuxPackages_latest.vmware.overrideAttrs (_oldAttrs: {
+            version = "workstation-17.5.2-k6.9+-unstable-2024-08-22";
+            src = final.fetchFromGitHub {
+              owner = "philipl";
+              repo = "vmware-host-modules";
+              rev = "6797e552638a28d1fa1e9ebd7ab5d3c628671ba0";
+              hash = "sha256-KCLxAF6UtNIdKcDoANviln2RJuz1Ld8jq5QFW9ONghs=";
+            };
+          });
+        }
+      );
+
       # TODO delete when https://github.com/NixOS/nixpkgs/issues/400317 gets resolved
       inherit (nixpkgs-jetbrains) jetbrains;
 

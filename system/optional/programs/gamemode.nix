dcc6b2db695b2daedb416fb617f201ce9877660f
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-19 14:52:08 +0200

    gamemode: disable hyprland kb_options when starting

--- system/optional/programs/gamemode.nix
+++ system/optional/programs/gamemode.nix
@@ -14,10 +14,20 @@
       custom =
         let
           notify = title: "${lib.getExe pkgs.libnotify} -a gamemoded -u critical -t 3000 '${title}'";
+
+          start-script = pkgs.writeShellScript "gamemode-start" ''
+            hyprctl getoption input:kb_options -j | ${lib.getExe pkgs.jq} ".str" -r > $XDG_RUNTIME_DIR/hypr-kb-options
+            hyprctl keyword input:kb_options ""
+            ${notify "Game Mode enabled!"}
+          '';
+          end-script = pkgs.writeShellScript "gamemode-end" ''
+            hyprctl keyword input:kb_options $(cat $XDG_RUNTIME_DIR/hypr-kb-options)
+            ${notify "Game Mode disabled!"}
+          '';
         in
         {
-          start = notify "Game Mode enabled!";
-          end = notify "Game Mode disabled!";
+          start = start-script.outPath;
+          end = end-script.outPath;
         };
     };
   };

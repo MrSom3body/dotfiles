dffcdf7d3c1907ded3eb904c0195a37f79c756e1
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-21 23:01:11 +0200

    ghostty: disable gtk single instance and add quit after window close delay

--- home/terminal/emulators/ghostty.nix
+++ home/terminal/emulators/ghostty.nix
@@ -10,7 +10,6 @@ let
 
   inherit (lib) mkEnableOption;
   cfg = config.my.terminal.emulators.ghostty;
-  cfg' = config.programs.ghostty;
 in
 {
   options.my.terminal.emulators.ghostty = {
@@ -23,30 +22,14 @@ in
       package = inputs.ghostty.packages.${pkgs.system}.default;
       settings = {
         font-feature = [ "ss06" ];
-        gtk-single-instance = true;
         mouse-hide-while-typing = true;
         window-decoration = false;
         window-padding-balance = true;
         window-padding-x = 15;
         window-padding-y = 15;
-      };
-    };
 
-    systemd.user.services.ghostty = {
-      Unit = {
-        Description = "Fast, native, feature-rich terminal emulator pushing modern features.";
-        Documentation = "man:ghostty(1)";
-        PartOf = [ "graphical-session.target" ];
-        After = [ "graphical-session.target" ];
-      };
-
-      Service = {
-        ExecStart = "${cfg'.package}/bin/ghostty --gtk-single-instance=true --initial-window=false --quit-after-last-window-closed=false";
-        Restart = "on-failure";
-      };
-
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
+        quit-after-last-window-closed = true;
+        quit-after-last-window-closed-delay = "5m";
       };
     };
   };

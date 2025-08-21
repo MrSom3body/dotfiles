d33c8864518f67dc865cf51a3e4afb631847c5c1
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-22 01:01:25 +0200

    gamemode: fix start and end scripts

--- system/optional/programs/gamemode.nix
+++ system/optional/programs/gamemode.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  config,
   pkgs,
   ...
 }:
@@ -15,12 +16,33 @@
         let
           notify = title: "${lib.getExe pkgs.libnotify} -a gamemoded -u critical -t 3000 '${title}'";
 
+          programs = lib.makeBinPath (
+            (builtins.attrValues {
+              inherit (pkgs)
+                coreutils
+                power-profiles-daemon
+                systemd
+                libnotify
+                ;
+            })
+            ++ [
+              config.programs.hyprland.package
+            ]
+          );
+
+          init-script = # bash
+            ''
+              export PATH=$PATH:${programs}
+              export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /run/user/1000/hypr | tail -1)
+            '';
           start-script = pkgs.writeShellScript "gamemode-start" ''
+            ${init-script}
             hyprctl getoption input:kb_options -j | ${lib.getExe pkgs.jq} ".str" -r > $XDG_RUNTIME_DIR/hypr-kb-options
             hyprctl keyword input:kb_options ""
             ${notify "Game Mode enabled!"}
           '';
           end-script = pkgs.writeShellScript "gamemode-end" ''
+            ${init-script}
             hyprctl keyword input:kb_options $(cat $XDG_RUNTIME_DIR/hypr-kb-options)
             ${notify "Game Mode disabled!"}
           '';

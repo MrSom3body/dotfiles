1a5cf40adb1365da21eb560b26bf471f94a35b5b
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-09-04 09:21:04 +0200

    checks: exclude hardware-configuration.nix from checks

--- checks.nix
+++ checks.nix
@@ -6,12 +6,16 @@
 {
   pre-commit-check = inputs.git-hooks-nix.lib.${pkgs.system}.run {
     src = ./.;
+    excludes = [ "hardware-configuration.nix" ];
     hooks = {
       # nix
       deadnix.enable = true;
       nil.enable = true;
       nixfmt-rfc-style.enable = true;
-      statix.enable = true;
+      statix = {
+        enable = true;
+        settings.ignore = [ "hardware-configuration.nix" ];
+      };
 
       # markdown
       markdownlint = {

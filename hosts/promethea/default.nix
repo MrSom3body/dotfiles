fa5b2dab3d5a66b39c3edbabb484bcee235e1080
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-09-05 15:30:39 +0200

    promethea: temporarily create a custom nvidia-suspend-then-hibernate file

--- hosts/promethea/default.nix
+++ hosts/promethea/default.nix
@@ -1,4 +1,9 @@
-{ inputs, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./hardware-configuration.nix
@@ -26,6 +31,29 @@
   ++ [
     inputs.nixos-hardware.nixosModules.asus-zenbook-um6702
   ];
+
+  # TODO remove when https://github.com/NixOS/nixpkgs/pull/440422 gets merged
+  systemd.services = {
+    nvidia-suspend-then-hibernate =
+      let
+        state = "suspend-then-hibernate";
+      in
+      {
+        description = "NVIDIA system ${state} actions";
+        path = [ pkgs.kbd ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh "suspend"'';
+        };
+        before = [ "systemd-${state}.service" ];
+        requiredBy = [ "systemd-${state}.service" ];
+      };
+
+    nvidia-resume = {
+      after = [ "systemd-suspend-then-hibernate.service" ];
+      requiredBy = [ "systemd-suspend-then-hibernate.service" ];
+    };
+  };
 
   my = {
     boot.lanzaboote.enable = true;

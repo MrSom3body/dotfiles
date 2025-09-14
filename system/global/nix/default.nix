441067bf746168a76ef6b0b5864726cd0b3f5dfd
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-09-14 13:27:22 +0200

    nix: allow the use of every flake in nix commands

--- system/global/nix/default.nix
+++ system/global/nix/default.nix
@@ -13,35 +13,39 @@
 
   environment.systemPackages = [ pkgs.git ];
 
-  nix = {
-    package = pkgs.lix;
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
+    in
+    {
+      package = pkgs.lix;
 
-    # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
-    registry.nixpkgs.flake = inputs.nixpkgs;
-    channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
+      # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+      registry = lib.mapAttrs (_: v: { flake = v; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
+      channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
 
-    settings = {
-      accept-flake-config = lib.mkForce false;
-      log-lines = lib.mkDefault 25; # more log lines
+      settings = {
+        accept-flake-config = lib.mkForce false;
+        log-lines = lib.mkDefault 25; # more log lines
 
-      builders-use-substitutes = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+        builders-use-substitutes = true;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
 
-      keep-derivations = true;
-      keep-outputs = true;
+        keep-derivations = true;
+        keep-outputs = true;
 
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
+        trusted-users = [
+          "root"
+          "@wheel"
+        ];
+      };
+
+      optimise.automatic = !config.boot.isContainer;
     };
-
-    optimise.automatic = !config.boot.isContainer;
-  };
 
   nixpkgs = {
     config.allowUnfree = true;

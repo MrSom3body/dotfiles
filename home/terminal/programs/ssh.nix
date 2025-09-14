a7d1a23ab7f75c600151466d4643ab6cc4bc4484
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-09-14 12:41:11 +0200

    ssh: disable deprecated enableDefaultConfig option

--- home/terminal/programs/ssh.nix
+++ home/terminal/programs/ssh.nix
@@ -16,7 +16,18 @@ in
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
         setEnv = {
           TERM = "xterm-256color";
         };

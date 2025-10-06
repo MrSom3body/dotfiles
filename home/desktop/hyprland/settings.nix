9fe5d91a6acbe315bb6e32f0825b5d6bcb3fa2b3
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-10-06 13:15:43 +0200

    hyprland/settings: invert workspace scroll

--- home/desktop/hyprland/settings.nix
+++ home/desktop/hyprland/settings.nix
@@ -90,7 +90,7 @@ in
         };
 
         gestures = {
-          workspace_swipe_create_new = true;
+          workspace_swipe_invert = false;
           workspace_swipe_direction_lock = false;
           workspace_swipe_forever = true;
           workspace_swipe_use_r = true;

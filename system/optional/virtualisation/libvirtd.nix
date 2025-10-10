b3a28eef2683f6f7709e91d57bd5a4947e9382ea
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-10-10 23:32:47 +0200

    libvirtd: remove soon to be deprecated ovmf options

--- system/optional/virtualisation/libvirtd.nix
+++ system/optional/virtualisation/libvirtd.nix
@@ -11,15 +11,6 @@
         runAsRoot = false;
         vhostUserPackages = [ pkgs.virtiofsd ];
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
-        };
       };
     };
     spiceUSBRedirection.enable = true;

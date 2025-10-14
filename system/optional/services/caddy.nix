2a19027062ae0ed9e4b42a094bdc2046d27c5ad4
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-10-14 06:35:29 +0200

    caddy: update plugins

--- system/optional/services/caddy.nix
+++ system/optional/services/caddy.nix
@@ -22,7 +22,7 @@
       '';
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
+        hash = "sha256-XwZ0Hkeh2FpQL/fInaSq+/3rCLmQRVvwBM0Y1G1FZNU=";
       };
     };
   };

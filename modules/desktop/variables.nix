b693433e4c6937875d1f9c4ff619f80de4457eea
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-10-21 07:35:24 +0200

    desktop/variables: set EDITOR and BROWSER to meta.programs.*

--- modules/desktop/variables.nix
+++ modules/desktop/variables.nix
@@ -1,8 +1,12 @@
+{ config, ... }:
+let
+  inherit (config.flake.meta) programs;
+in
 {
   flake.modules.homeManager.desktop = {
     home.sessionVariables = {
-      EDITOR = "hx";
-      BROWSER = "zen";
+      EDITOR = programs.editor;
+      BROWSER = programs.browser;
     };
   };
 }

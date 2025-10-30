c335e3390341469a401cccf050799782d7f03d1b
Karun Sandhu <karun@sndh.dev>
2025-10-30 12:46:35 +0100

    desktop/gnome-keyring: move as much configuration as possible to home-manager

--- modules/desktop/gnome-keyring.nix
+++ modules/desktop/gnome-keyring.nix
@@ -1,12 +1,28 @@
 {
-  flake.modules.nixos.desktop = {
-    services.gnome.gnome-keyring.enable = true;
-    programs.seahorse.enable = true;
-
-    security.pam.services = {
-      greetd.enableGnomeKeyring = true;
-      greetd-password.enableGnomeKeyring = true;
-      login.enableGnomeKeyring = true;
+  flake.modules = {
+    nixos.desktop = {
+      security.pam.services = {
+        greetd.enableGnomeKeyring = true;
+        greetd-password.enableGnomeKeyring = true;
+        login.enableGnomeKeyring = true;
+      };
     };
+
+    homeManager.desktop =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          gcr
+          seahorse
+        ];
+
+        services.gnome-keyring = {
+          enable = true;
+          components = [
+            "pkcs11"
+            "secrets"
+          ];
+        };
+      };
   };
 }

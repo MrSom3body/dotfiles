5a5565914d0d5be6ff0e29946cbe6d02acbbfeae
Karun Sandhu <karun@sndh.dev>
2025-10-30 12:46:56 +0100

    desktop/polkit: use home-manager service instead of writing own

--- modules/desktop/services/polkit.nix
+++ modules/desktop/services/polkit.nix
@@ -1,25 +1,5 @@
 {
-  flake.modules.homeManager.desktop =
-    { config, pkgs, ... }:
-    {
-      systemd.user.services.polkit-gnome-authentication-agent-1 = {
-        Unit = {
-          Description = "polkit-gnome-authentication-agent-1";
-          Wants = [ config.wayland.systemd.target ];
-          After = [ config.wayland.systemd.target ];
-        };
-
-        Install = {
-          WantedBy = [ config.wayland.systemd.target ];
-        };
-
-        Service = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
-    };
+  flake.modules.homeManager.desktop = {
+    services.polkit-gnome.enable = true;
+  };
 }

4a8a63b0eb8d19e2fab55d57871747e233552878
Karun Sandhu <karun@sndh.dev>
2025-11-17 12:02:50 +0100

    media/streaming: replace jellyfin client with tsukimi

--- modules/hosts/promethea/nixpkgs.nix
+++
@@ -1,5 +0,0 @@
-{
-  flake.modules.nixos."hosts/promethea" = {
-    nixpkgs.config.permittedInsecurePackages = [ "qtwebengine-5.15.19" ]; # TODO remove https://github.com/NixOS/nixpkgs/issues/437865
-  };
-}
--- modules/media/streaming.nix
+++ modules/media/streaming.nix
@@ -4,7 +4,7 @@
     {
       home.packages = with pkgs; [
         grayjay
-        jellyfin-media-player
+        tsukimi
       ];
     };
 }

ae5a14b565e6595d765655a74eb249a6412a808f
Karun Sandhu <karun@sndh.dev>
2025-11-25 17:03:10 +0100

    caddy: update plugins

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -20,7 +20,8 @@
           '';
           package = pkgs.caddy.withPlugins {
             plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-            hash = "sha256-XwZ0Hkeh2FpQL/fInaSq+/3rCLmQRVvwBM0Y1G1FZNU=";
+            hash = "sha256-Dvifm7rRwFfgXfcYvXcPDNlMaoxKd5h4mHEK6kJ+T4A=
+";
           };
         };
       };

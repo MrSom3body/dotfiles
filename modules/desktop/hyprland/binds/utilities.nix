b7bf36796319ba42f2a5c29a67db6fd61d44dab5
Karun Sandhu <karun@sndh.dev>
2025-12-09 19:33:20 +0100

    hyprland: replace pyprland zoom with hyprland native zoom

--- modules/desktop/hyprland/binds/utilities.nix
+++ modules/desktop/hyprland/binds/utilities.nix
@@ -1,4 +1,4 @@
-_: {
+{
   flake.modules.homeManager.desktop = {
     wayland.windowManager.hyprland.settings = {
       bindd =
---
+++ modules/desktop/hyprland/binds/zoom.nix
@@ -0,0 +1,22 @@
+{ lib, ... }:
+{
+  flake.modules.homeManager.desktop =
+    { pkgs, ... }:
+    {
+      wayland.windowManager.hyprland.settings = {
+        bindd = [
+          # Reset Zoom
+          "SUPER SHIFT, minus, Reset zoom, exec, hyprctl -q keyword cursor:zoom_factor 1"
+        ];
+
+        bindde =
+          let
+            jq = lib.getExe pkgs.jq;
+          in
+          [
+            "SUPER, plus, Zoom in, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | ${jq} '.float * 1.1')"
+            "SUPER, minus, Zoom out, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | ${jq} '(.float * 0.9) | if . < 1 then 1 else . end')"
+          ];
+      };
+    };
+}

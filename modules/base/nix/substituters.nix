0de55f8be74ca95a09c4709ddcf42d9057dfa780
Karun Sandhu <karun@sndh.dev>
2025-12-13 17:34:50 +0100

    nix/substituters: only use nixpkgs & garnix cache

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -4,26 +4,10 @@
       substituters = [
         "https://cache.nixos.org?priority=10"
         "https://cache.garnix.io"
-        "https://nix-community.cachix.org"
-
-        "https://cuda-maintainers.cachix.org"
-        "https://ghostty.cachix.org"
-        "https://gotcha.cachix.org"
-        "https://helix.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://yazi.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-        "gotcha.cachix.org-1:HvkCMDHVThJj/aaxBgDvjraKYSNM1yFKveTsPYG3YUA="
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       ];
     };
   };

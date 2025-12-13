200a3546dcdb35d7fa5c3b0998b4ad48e554ba70
Karun Sandhu <karun@sndh.dev>
2025-12-13 16:50:54 +0100

    hyprpaper: temporarily set wallpaper option manually
    see https://github.com/nix-community/stylix/pull/2087

--- modules/desktop/services/hyprpaper.nix
+++ modules/desktop/services/hyprpaper.nix
@@ -1,12 +1,20 @@
-{ inputs, ... }:
+{ lib, inputs, ... }:
 {
   flake.modules.homeManager.desktop =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
       services.hyprpaper = {
         enable = true;
 
         package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+        # TODO remove when https://github.com/nix-community/stylix/pull/2087 gets merged
+        settings.wallpaper = lib.mkForce (
+          lib.singleton {
+            monitor = "";
+            path = toString config.stylix.image;
+          }
+        );
       };
     };
 }

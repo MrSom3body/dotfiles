2da464dc893eb164721aa3d1896a0a5faf6ec3a1
Karun Sandhu <karun@sndh.dev>
2025-12-24 12:38:02 +0100

    ssh: add lbt host

--- modules/base/ssh.nix
+++ modules/base/ssh.nix
@@ -3,19 +3,25 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-        setEnv = {
-          TERM = "xterm-256color";
+      matchBlocks = {
+        "lbt" = {
+          hostname = "lbt.tail7807a4.ts.net";
+        };
+
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
       };
     };

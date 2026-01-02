4491507f1b715d1a79032ce5eef7b8cd77732d32
Karun Sandhu <karun@sndh.dev>
2026-01-03 00:21:35 +0100

    tuned: disable panel power savings
    makes colors look washed out

--- modules/desktop/power.nix
+++ modules/desktop/power.nix
@@ -2,10 +2,19 @@
 {
   flake.modules.nixos.desktop = {
     services = {
-      tuned.enable = true;
+      tuned = {
+        enable = true;
+        profiles = {
+          panel_power_savings.video.panel_power_savings = 0; # makes colors look washed out
+        };
+      };
       tlp.enable = lib.mkForce false;
 
       upower.enable = true;
+    };
+
+    environment.etc = {
+      "tuned/post_loaded_profile".text = "panel_power_savings";
     };
   };
 }

7f0f7fd21c0408ab46d0652119b77f1e0c751c14
Karun Sandhu <karun@sndh.dev>
2026-01-03 13:46:10 +0100

    gaming: remove custom desktop entry for modrinth

--- modules/gaming/gaming.nix
+++ modules/gaming/gaming.nix
@@ -13,29 +13,6 @@
             r2modman
             ;
         };
-
-        # TODO remove when modrinth works normally again on wayland
-        xdg.desktopEntries = {
-          "Modrinth App" = {
-            categories = [
-              "Game"
-              "ActionGame"
-              "AdventureGame"
-              "Simulation"
-            ];
-            exec = ''sh -c "GDK_BACKEND=x11 ModrinthApp"'';
-            icon = "ModrinthApp";
-            name = "Modrinth App";
-            terminal = false;
-            type = "Application";
-            mimeType = [
-              "application/x-modrinth-modpack+zip"
-              "x-scheme-handler/modrinth"
-            ];
-            comment = "Modrinth's game launcher";
-            startupNotify = true;
-          };
-        };
       };
   };
 }

185777ed91d48c4d4312f7f299dd72d75e805f33
Karun Sandhu <karun@sndh.dev>
2026-01-05 18:09:03 +0100

    overlays: add overlay for solaar

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -9,6 +9,22 @@
         '';
       });
 
+      # TODO remove when https://github.com/NixOS/nixpkgs/pull/477215 lands in unstable
+      solaar = prev.solaar.overrideAttrs (_oldAttrs: rec {
+        version = "1.1.18";
+        src = final.fetchFromGitHub {
+          owner = "pwr-Solaar";
+          repo = "Solaar";
+          tag = version;
+          hash = "sha256-K1mh1FgdYe1yioczUoOb7rrI0laq+1B4TLlblerMyHE=";
+        };
+
+        preConfigure = ''
+          substituteInPlace lib/solaar/listener.py \
+            --replace-fail getfacl "${final.lib.getExe' final.acl "getfacl"}"
+        '';
+      });
+
       # TODO remove when https://github.com/nixos/nixpkgs/pull/476229 lands in unstable
       readline70 = prev.readline70.overrideAttrs (_oldAttrs: {
         env.NIX_CFLAGS_COMPILE = final.lib.optionalString final.stdenv.cc.isGNU "-std=gnu17";

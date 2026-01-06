464d49a0fc592850ab0f8934f51edc90093fa5aa
Karun Sandhu <karun@sndh.dev>
2026-01-06 21:47:58 +0100

    hyprpaper: fix not setting wallpaper

--- modules/desktop/services/hyprpaper.nix
+++ modules/desktop/services/hyprpaper.nix
@@ -1,7 +1,17 @@
+{ lib, ... }:
 {
-  flake.modules.homeManager.desktop = {
-    services.hyprpaper = {
-      enable = true;
+  flake.modules.homeManager.desktop =
+    { config, ... }:
+    {
+      services.hyprpaper = {
+        enable = true;
+        # TODO remove when https://github.com/nix-community/stylix/pull/2087 gets merged
+        settings.wallpaper = lib.mkForce (
+          lib.singleton {
+            monitor = "";
+            path = toString config.stylix.image;
+          }
+        );
+      };
     };
-  };
 }

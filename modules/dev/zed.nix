b226751426fe46faefcea2549296855be16c1892
Karun Sandhu <karun@sndh.dev>
2026-01-18 04:08:06 +0100

    zed: add more configuration

--- modules/dev/zed.nix
+++ modules/dev/zed.nix
@@ -1,23 +1,52 @@
 {
-  flake.modules.homeManager.dev = {
-    programs.zed-editor = {
-      enable = true;
-      extensions = [ "nix" ];
-      userSettings = {
-        agent = {
-          default_model = {
-            provider = "copilot_chat";
-            model = "claude-opus-4.5";
+
+  self,
+  lib,
+  ...
+}:
+{
+  flake.modules.homeManager.dev =
+    { osConfig, pkgs, ... }:
+    {
+      programs.zed-editor = {
+        enable = true;
+        extensions = [
+          "django"
+          "nix"
+        ];
+        userSettings = {
+          agent = {
+            default_model = {
+              provider = "copilot_chat";
+              model = "claude-opus-4.5";
+            };
+          };
+          helix_mode = true;
+          telemetry = {
+            diagnostics = false;
+            metrics = false;
+          };
+          base_keymap = "Atom";
+          load_direnv = "shell_hook";
+
+          lsp = {
+            nixd = {
+              binary.path = lib.getExe pkgs.nixd;
+              settings = {
+                formatting.command = [ (lib.getExe pkgs.nixfmt) ];
+                options =
+                  let
+                    flake = ''(builtins.getFlake "${self}")'';
+                    nixos-expr = "${flake}.nixosConfigurations.${osConfig.networking.hostName}.options";
+                  in
+                  {
+                    nixos.expr = nixos-expr;
+                    home-manager.expr = "${nixos-expr}.home-manager.users.type.getSubOptions []";
+                  };
+              };
+            };
           };
         };
-        helix_mode = true;
-        telemetry = {
-          diagnostics = false;
-          metrics = false;
-        };
-        base_keymap = "Atom";
-        load_direnv = "shell_hook";
       };
     };
-  };
 }

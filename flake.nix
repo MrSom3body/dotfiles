441692a42fda0eaac830fab30ea10badc60579b3
Karun Sandhu <karun@sndh.dev>
2026-01-21 20:55:54 +0100

    flake: use my fork for crillios-ls

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
 
     # programs
     crillios-ls = {
-      url = "github:Crill-IOS/Crill-IOS-Scripting";
+      url = "github:MrSom3body-contrib/Crill-IOS-Scripting/fix/package-overrides-for-nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         git-hooks-nix.follows = "git-hooks-nix";

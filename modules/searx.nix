328c94c21444808a96da3c152b5897aff159d771
Karun Sandhu <karun@sndh.dev>
2026-02-15 00:51:40 +0100

    searx: enable image proxy

--- modules/searx.nix
+++ modules/searx.nix
@@ -32,6 +32,7 @@ in
               bind_address = "127.0.0.1";
               secret_key = "@SEARX_SECRET_KEY@";
               method = "GET";
+              image_proxy = true;
             };
 
             general = {

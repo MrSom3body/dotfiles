10d27d6b6223bc7b528c1afbe247041fe1dd6816
Karun Sandhu <karun@sndh.dev>
2026-02-17 23:00:16 +0100

    iso: disable auto-upgrades

--- modules/iso/overrides.nix
+++ modules/iso/overrides.nix
@@ -3,8 +3,10 @@ let
   inherit (lib) mkForce;
 in
 {
-  # overrides needed for my config to work with the iso template
   flake.modules.nixos.iso = {
+    system.autoUpgrade.enable = mkForce false; # really not needed on an ISO
+
+    # overrides needed for my config to work with the iso template
     boot.supportedFilesystems.zfs = mkForce false;
     networking.wireless.enable = mkForce false;
     security = {

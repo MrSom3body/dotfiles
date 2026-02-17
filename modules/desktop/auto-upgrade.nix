a2d986286063f3f79df32500eea913a3a3436f62
Karun Sandhu <karun@sndh.dev>
2026-02-17 22:59:22 +0100

    auto-upgrade: always run (ain't no way I'm working at 5:00 in the morning)

--- modules/desktop/auto-upgrade.nix
+++ modules/desktop/auto-upgrade.nix
@@ -1,8 +1,8 @@
-{ self, config, ... }:
+{ config, ... }:
 {
   flake.modules.nixos.desktop = {
     system.autoUpgrade = {
-      enable = self ? rev;
+      enable = true;
       flake = config.flake.meta.uri;
       upgrade = false;
       operation = "boot";

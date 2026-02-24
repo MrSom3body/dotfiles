e26add6ff94eeacd2214b1a6da1a71a5f28e6a95
Karun Sandhu <karun@sndh.dev>
2026-02-24 21:05:55 +0100

    gemini-cli: even more settings

--- modules/gemini-cli.nix
+++ modules/gemini-cli.nix
@@ -1,4 +1,7 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
+let
+  inherit (config.flake.meta) programs;
+in
 {
   flake.modules.homeManager.gemini-cli =
     { pkgs, ... }:
@@ -7,19 +10,30 @@
         enable = true;
         package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.gemini-cli;
         settings = {
+          ui = {
+            hidebanner = true;
+            footer.hideContextPercentage = false;
+            inlineThinkingMode = "full";
+            showCitations = true;
+            showModelInfoInChat = true;
+            showStatusInTitle = true;
+          };
+
           general = {
+            preferredEditor = programs.editor;
             previewFeatures = true;
             vimMode = true;
+            checkpointing.enabled = true;
           };
-          ui.hidebanner = true;
-          privacy.usageStatisticsEnabled = false;
+          context.discoveryMaxDirs = 1000;
+          tools.shell.showColor = true;
           security = {
             disableYoloMode = true;
             auth.selectedType = "oauth-personal";
           };
-          experimental = {
-            plan = true;
-          };
+          experimental.plan = true;
+
+          privacy.usageStatisticsEnabled = false;
         };
       };
     };

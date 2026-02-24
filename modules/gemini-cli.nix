4ce77f4436be3b3336c8f1dc2579ff7587effc06
Karun Sandhu <karun@sndh.dev>
2026-02-24 21:12:50 +0100

    gemini-cli: use alternate buffer to keep shell history

--- modules/gemini-cli.nix
+++ modules/gemini-cli.nix
@@ -17,6 +17,7 @@ in
             showCitations = true;
             showModelInfoInChat = true;
             showStatusInTitle = true;
+            useAlternateBuffer = true;
           };
 
           general = {

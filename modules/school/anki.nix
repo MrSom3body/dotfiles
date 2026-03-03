1d93071ec39e656c0d135fdf5d73f351d810648c
Karun Sandhu <karun@sndh.dev>
2026-03-03 11:39:44 +0100

    school/anki: fix deprecations

--- modules/school/anki.nix
+++ modules/school/anki.nix
@@ -9,10 +9,13 @@
 
       programs.anki = {
         enable = true;
-        sync = {
-          autoSync = true;
-          usernameFile = config.sops.secrets.anki-username.path;
-          keyFile = config.sops.secrets.anki-key.path;
+        profiles."User 1" = {
+          default = true;
+          sync = {
+            autoSync = true;
+            usernameFile = config.sops.secrets.anki-username.path;
+            keyFile = config.sops.secrets.anki-key.path;
+          };
         };
       };
     };

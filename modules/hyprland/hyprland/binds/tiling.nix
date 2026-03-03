a389c7eb322a7ca0fcd87c4ff4552fb7a31525b3
Karun Sandhu <karun@sndh.dev>
2026-03-03 11:36:21 +0100

    hyprland/binds: use Super + M for promoting window

--- modules/hyprland/hyprland/binds/tiling.nix
+++ modules/hyprland/hyprland/binds/tiling.nix
@@ -69,7 +69,7 @@
           "SUPER, K, Focus window above, layoutmsg, focus u"
           "SUPER, L, Focus column to the right, layoutmsg, focus r"
 
-          # Move window/column (Mod+Ctrl in niri)
+          # Move window/column
           "SUPER SHIFT, H, Move column left, layoutmsg, swapcol l"
           "SUPER SHIFT, J, Move window down in column, swapwindow, d"
           "SUPER SHIFT, K, Move window up in column, swapwindow, u"
@@ -81,11 +81,10 @@
           "SUPER CTRL, K, Increase window size to the top, resizeactive, 0 -100"
           "SUPER CTRL, L, Increase column size, layoutmsg, colresize +0.2"
 
-          # Consume/expel windows (Mod+BracketLeft/Right in niri)
-          "SUPER, BracketRight, Expel window from column, layoutmsg, promote"
-          "SUPER, Period, Expel window from column, layoutmsg, promote"
+          # Promote windows
+          "SUPER, M, Expel window from column, layoutmsg, promote"
 
-          # Cycle preset column widths (Mod+R in niri)
+          # Cycle preset column widths
           "SUPER, R, Cycle column width, layoutmsg, colresize +conf"
           "SUPER SHIFT, R, Cycle column width, layoutmsg, colresize -conf"
 
@@ -93,7 +92,7 @@
           "SUPER, F, Maximize focused column, fullscreen, 1"
           "SUPER SHIFT, F, Fullscreen focused window, fullscreen, 0"
 
-          # Center focused column (Mod+C in niri)
+          # Center focused column
           "SUPER, C, Toggle center/fit focused column, layoutmsg, togglefit"
         ];
       };

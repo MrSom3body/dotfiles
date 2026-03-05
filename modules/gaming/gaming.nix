31e919bab66557be8017a8e5ce874840697ac697
Karun Sandhu <karun@sndh.dev>
2026-03-05 21:44:42 +0100

    gaming: use unstable modrinth again

--- modules/gaming/gaming.nix
+++ modules/gaming/gaming.nix
@@ -4,17 +4,15 @@
     homeManager.gaming =
       { pkgs, ... }:
       {
-        home.packages =
-          builtins.attrValues {
-            inherit (pkgs)
-              goverlay
-              heroic
-              # mindustry-wayland
-              # modrinth-app
-              r2modman
-              ;
-          }
-          ++ [ pkgs.stable.modrinth-app ]; # TODO remove when https://nixpkgs-tracker.ocfox.me/?pr=485074 lands in nixos-unstable
+        home.packages = builtins.attrValues {
+          inherit (pkgs)
+            goverlay
+            heroic
+            # mindustry-wayland
+            modrinth-app
+            r2modman
+            ;
+        };
       };
   };
 }

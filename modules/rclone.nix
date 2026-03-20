a3fafaa6707f319fefcb906e5e94773891f887ac
Karun Sandhu <karun@sndh.dev>
2026-03-21 00:50:01 +0100

    rclone: disable auto backup

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -1,69 +1,64 @@
 {
   flake.modules.homeManager.rclone =
-    {
-      osConfig,
-      config,
-      pkgs,
-      ...
-    }:
+    { pkgs, ... }:
     {
       home.packages = [ pkgs.rclone ];
 
-      systemd.user =
-        let
-          unitName = "rclone-proton-drive-backup";
-          mountpoint = "proton:Computers/${osConfig.networking.hostName}";
-          filterFile = pkgs.writeText "rclone-filters" ''
-            - {{\.?venv}}/**
-            - .devenv/**
-            - .direnv/**
-            - /Documents/Codes/nixpkgs/**
-            - /Documents/Schule/2024-25/INSY/oracle-volume/**
-            + /Desktop/**
-            + /Documents/**
-            + /Games/Saves/**
-            + /Music/**
-            + /Pictures/**
-            + /Templates/**
-            + /Videos/**
-            - *
-          '';
-        in
-        {
-          services.${unitName} = {
-            Unit.Description = "Sync some folders to Proton Drive automatically";
+      # systemd.user =
+      #   let
+      #     unitName = "rclone-proton-drive-backup";
+      #     mountpoint = "proton:Computers/${osConfig.networking.hostName}";
+      #     filterFile = pkgs.writeText "rclone-filters" ''
+      #       - {{\.?venv}}/**
+      #       - .devenv/**
+      #       - .direnv/**
+      #       - /Documents/Codes/nixpkgs/**
+      #       - /Documents/Schule/2024-25/INSY/oracle-volume/**
+      #       + /Desktop/**
+      #       + /Documents/**
+      #       + /Games/Saves/**
+      #       + /Music/**
+      #       + /Pictures/**
+      #       + /Templates/**
+      #       + /Videos/**
+      #       - *
+      #     '';
+      #   in
+      #   {
+      #     services.${unitName} = {
+      #       Unit.Description = "Sync some folders to Proton Drive automatically";
 
-            Service = {
-              Type = "oneshot";
-              ExecStartPre = pkgs.writeShellScript "waitForNetwork" ''
-                while ! ${pkgs.inetutils}/bin/ping -c 1 9.9.9.9; do
-                  sleep 1
-                done
-              '';
-              ExecStart = pkgs.writeShellScript "rcloneProtonDriveBackupScript" ''
-                ${pkgs.rclone}/bin/rclone --config=.config/rclone/rclone.conf \
-                  sync ${config.home.homeDirectory} ${mountpoint}/ \
-                  --filter-from ${filterFile} \
-                  -vv \
-                  --ignore-errors \
-                  --protondrive-replace-existing-draft=true
-              '';
-            };
-          };
+      #       Service = {
+      #         Type = "oneshot";
+      #         ExecStartPre = pkgs.writeShellScript "waitForNetwork" ''
+      #           while ! ${pkgs.inetutils}/bin/ping -c 1 9.9.9.9; do
+      #             sleep 1
+      #           done
+      #         '';
+      #         ExecStart = pkgs.writeShellScript "rcloneProtonDriveBackupScript" ''
+      #           ${pkgs.rclone}/bin/rclone --config=.config/rclone/rclone.conf \
+      #             sync ${config.home.homeDirectory} ${mountpoint}/ \
+      #             --filter-from ${filterFile} \
+      #             -vv \
+      #             --ignore-errors \
+      #             --protondrive-replace-existing-draft=true
+      #         '';
+      #       };
+      #     };
 
-          timers.${unitName} = {
-            Unit = {
-              Description = "Proton Drive Backup Timer";
-            };
+      #     timers.${unitName} = {
+      #       Unit = {
+      #         Description = "Proton Drive Backup Timer";
+      #       };
 
-            Timer = {
-              OnCalendar = "daily";
-              Unit = "${unitName}.service";
-              Persistent = true;
-            };
+      #       Timer = {
+      #         OnCalendar = "daily";
+      #         Unit = "${unitName}.service";
+      #         Persistent = true;
+      #       };
 
-            Install.WantedBy = [ "timers.target" ];
-          };
-        };
+      #       Install.WantedBy = [ "timers.target" ];
+      #     };
+      #   };
     };
 }

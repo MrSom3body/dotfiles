40a848c8ef2a26dd2f08bcaca90bb78a52cee18d
Karun Sandhu <karun@sndh.dev>
2026-03-31 14:37:31 +0200

    desktop/syncthing: use correct modules

--- modules/desktop/services/syncthing.nix
+++ modules/desktop/services/syncthing.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   flake.modules = {
-    nixos.desktop.imports = [ config.flake.modules.nixos.syncthing-server ];
+    nixos.desktop.imports = [ config.flake.modules.nixos.syncthing ];
     homeManager.desktop.imports = [ config.flake.modules.homeManager.syncthing ];
   };
 }

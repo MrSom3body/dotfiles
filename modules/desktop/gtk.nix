1b86c6292789ed363d722e52fc355e1a7ba3bdd2
Karun Sandhu <karun@sndh.dev>
2026-04-02 19:12:42 +0200

    gtk: set gtk4 theme to null

---
+++ modules/desktop/gtk.nix
@@ -0,0 +1,21 @@
+{
+  flake.modules.homeManager.desktop =
+    { config, ... }:
+    {
+      gtk = {
+        gtk4.theme = null;
+        gtk3.bookmarks = map (dir: "file://${config.home.homeDirectory}/" + dir) [
+          "Desktop"
+          "Documents"
+          "Downloads"
+          "Games"
+          "Music"
+          "Videos"
+          "dotfiles"
+          "Documents/Codes"
+          "Documents/Notes"
+          "Documents/Schule/2025-26"
+        ];
+      };
+    };
+}

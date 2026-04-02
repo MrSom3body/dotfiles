9c11a58965db3fbbdf78328cbf1868e22b48a1ab
Karun Sandhu <karun@sndh.dev>
2026-04-02 20:04:02 +0200

    wakapi: enable anubis

--- modules/wakapi.nix
+++ modules/wakapi.nix
@@ -3,69 +3,68 @@ let
   inherit (config.flake) meta;
 in
 {
-  flake = {
-    modules.nixos.wakapi =
-      { config, ... }:
-      {
-        sops.secrets.wakapi = {
-          sopsFile = ../secrets/wakapi.env;
-          format = "dotenv";
-          owner = "wakapi";
-          group = "wakapi";
-        };
-
-        services = {
-          wakapi = {
-            enable = true;
-            environmentFiles = [ config.sops.secrets.wakapi.path ];
-            database.createLocally = true;
-            settings = {
-              server = {
-                inherit (meta.services.wakapi) port;
-                public_url = "https://${meta.services.wakapi.domain}";
-              };
-
-              db = {
-                dialect = "postgres";
-                host = "/run/postgresql";
-                port = 5432; # this needs to be set otherwise the service will fail
-                name = "wakapi";
-                user = "wakapi";
-              };
-
-              security = {
-                insecure_cookies = false;
-                allow_signup = false;
-                disable_frontpage = true;
-              };
-
-              mail =
-                let
-                  mail = "noreply@${config.networking.domain}";
-                in
-                {
-                  enabled = true;
-                  provider = "smtp";
-                  sender = "Wakapi <${mail}>";
-                  smtp = {
-                    host = "smtp.protonmail.ch";
-                    port = 587;
-                    username = mail;
-                    tls = false;
-                  };
-                };
-            };
-          };
-
-          caddy.virtualHosts = {
-            ${meta.services.wakapi.domain} = {
-              extraConfig = ''
-                reverse_proxy http://localhost:${toString meta.services.wakapi.port}
-                tls internal
-              '';
-            };
-          };
-        };
+  flake.modules.nixos.wakapi =
+    { config, ... }:
+    let
+      anubisCfg = config.services.anubis.instances.wakapi.settings;
+    in
+    {
+      sops.secrets.wakapi = {
+        sopsFile = ../secrets/wakapi.env;
+        format = "dotenv";
+        owner = "wakapi";
+        group = "wakapi";
       };
-  };
+
+      services = {
+        anubis.instances.wakapi.settings.TARGET = "http://127.0.0.1:${toString meta.services.wakapi.port}";
+
+        wakapi = {
+          enable = true;
+          environmentFiles = [ config.sops.secrets.wakapi.path ];
+          database.createLocally = true;
+          settings = {
+            server = {
+              inherit (meta.services.wakapi) port;
+              public_url = "https://${meta.services.wakapi.domain}";
+            };
+
+            db = {
+              dialect = "postgres";
+              host = "/run/postgresql";
+              port = 5432; # this needs to be set otherwise the service will fail
+              name = "wakapi";
+              user = "wakapi";
+            };
+
+            security = {
+              insecure_cookies = false;
+              allow_signup = false;
+              disable_frontpage = true;
+            };
+
+            mail =
+              let
+                mail = "noreply@${config.networking.domain}";
+              in
+              {
+                enabled = true;
+                provider = "smtp";
+                sender = "Wakapi <${mail}>";
+                smtp = {
+                  host = "smtp.protonmail.ch";
+                  port = 587;
+                  username = mail;
+                  tls = false;
+                };
+              };
+          };
+        };
+
+        caddy.virtualHosts.${meta.services.wakapi.domain}.extraConfig = ''
+          reverse_proxy unix/${anubisCfg.BIND}
+          tls internal
+        '';
+      };
+    };
 }

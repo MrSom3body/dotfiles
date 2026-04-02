b9c59554bb5a36e7da5f19fac4aac6ae7ba60607
Karun Sandhu <karun@sndh.dev>
2026-04-02 19:13:01 +0200

    xdg: set session variables for user dirs

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -113,6 +113,7 @@ in
 
           userDirs = {
             enable = true;
+            setSessionVariables = true;
             createDirectories = true;
             extraConfig = {
               SCREENSHOTS = "${config.xdg.userDirs.pictures}/Screenshots";

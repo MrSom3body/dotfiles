03e553835b07dce3da4216f143fc4238a36b7a6e
Karun Sandhu <karun@sndh.dev>
2026-04-08 22:17:49 +0200

    hosts/prometea: vibing

--- modules/hosts/promethea/vibing.nix
+++ modules/hosts/promethea/vibing.nix
@@ -11,13 +11,27 @@ in
           specialisation.vibing.configuration = {
             environment.etc."specialisation".text = "vibing"; # for nh
             system.nixos.tags = [ "vibing" ]; # to display it in the boot loader
-            imports = [ flakeConfig.flake.modules.nixos.ollama ];
+            imports = [
+              flakeConfig.flake.modules.nixos.ollama
+              # flakeConfig.flake.modules.nixos.hermes
+            ];
 
-            services.ollama = {
-              package = pkgs.ollama-rocm;
-              loadModels = [ "gemma4:26b-a4b-it-q4_K_M" ];
-              rocmOverrideGfx = "10.3.0";
-              environmentVariables.OLLAMA_CONTEXT_LENGTH = "32768";
+            services = {
+              ollama = {
+                package = pkgs.ollama-rocm;
+                loadModels = [ "gemma4:26b-a4b-it-q4_K_M" ];
+                rocmOverrideGfx = "10.3.0";
+                environmentVariables = {
+                  OLLAMA_CONTEXT_LENGTH = "32768";
+                  OLLAMA_FLASH_ATTENTION = "1";
+                };
+              };
+
+              # hermes-agent = {
+              #   settings = {
+              #     model.default = "gemma4:26b-a4b-it-q4_K_M";
+              #   };
+              # };
             };
           };
         };

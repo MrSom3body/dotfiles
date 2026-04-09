3b627ef8cdd8142833a7796f96cbbe65ebb75262
Karun Sandhu <karun@sndh.dev>
2026-04-09 23:35:50 +0200

    overlays: add deno

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -13,6 +13,19 @@
             --prefix PATH : ${final.lib.makeBinPath [ final.pandoc ]}
         '';
       });
+
+      # TODO remove when https://github.com/NixOS/nixpkgs/pull/506893 lands in unstable
+      deno = prev.deno.overrideAttrs (oldAttrs: {
+        patches =
+          (oldAttrs.patches or [ ])
+          ++ final.lib.optionals (final.stdenv.hostPlatform.isAarch64 && final.stdenv.hostPlatform.isLinux) [
+            (final.fetchpatch {
+              url = "https://github.com/denoland/deno/commit/fd331552de39501d47c43dc4b0c637b969402ab1.patch";
+              hash = "sha256-AIqLbTnBO2VUFiTumEZFORqSyfzB6chdvJQq8HeAM30=";
+            })
+          ];
+      });
+
     };
 
     stable-packages = final: _prev: {

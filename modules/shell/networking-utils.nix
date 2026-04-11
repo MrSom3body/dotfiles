c9dc90804d62dcd1d0b524e661afe887955ef0ba
Karun Sandhu <karun@sndh.dev>
2026-04-12 01:22:22 +0200

    shell/networking-utils: remove speedtest

--- modules/shell/networking-utils.nix
+++ modules/shell/networking-utils.nix
@@ -2,13 +2,6 @@
   flake.modules.homeManager.shell =
     { pkgs, ... }:
     {
-      home.packages = builtins.attrValues {
-        inherit (pkgs)
-          dig
-          iputils
-          nmap
-          speedtest-cli
-          ;
-      };
+      home.packages = builtins.attrValues { inherit (pkgs) dig iputils nmap; };
     };
 }

7f2f6508b348d3ce32cdfbc9f27b62ed8e31804b
Karun Sandhu <karun@sndh.dev>
2026-04-22 22:31:12 +0200

    office/contacts: init

---
+++ modules/office/contacts.nix
@@ -0,0 +1,44 @@
+{ config, ... }:
+let
+  inherit (config) flake;
+  inherit (flake) meta;
+  inherit (meta.users.karun) email;
+in
+{
+  flake.modules.homeManager.office =
+    { lib, config, ... }:
+    let
+      pass = lib.getExe config.programs.password-store.package;
+    in
+    {
+      accounts.contact = {
+        basePath = "${config.xdg.dataHome}/contacts";
+        accounts = {
+          contacts = {
+            khal = {
+              enable = true;
+              addresses = [ email ];
+              collections = [ "5ff7c5b5-503e-465d-bede-fc49b55f9168" ];
+            };
+            remote = {
+              type = "carddav";
+              url = "https://dav.sndh.dev";
+              userName = email;
+              passwordCommand = [
+                "${pass}"
+                "dav.sndh.dev/${email}"
+              ];
+            };
+            vdirsyncer = {
+              enable = true;
+              metadata = [
+                "color"
+                "displayname"
+              ];
+              collections = [ "5ff7c5b5-503e-465d-bede-fc49b55f9168" ];
+            };
+          };
+        };
+      };
+    };
+}

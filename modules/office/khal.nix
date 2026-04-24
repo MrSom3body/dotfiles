ac1e381d242abf103d0ec6f43d12ee5b68ede736
Karun Sandhu <karun@sndh.dev>
2026-04-24 13:11:40 +0200

    khal: add default event alarms

--- modules/office/khal.nix
+++ modules/office/khal.nix
@@ -22,7 +22,11 @@
         longdatetimeformat = "%c";
       };
       settings = {
-        default.highlight_event_days = true;
+        default = {
+          default_dayevent_alarm = "12h";
+          default_event_alarm = "30m";
+          highlight_event_days = true;
+        };
         highlight_days.color = "light blue";
       };
     };

69e056cb5afe5dca6b115cf2b0d928b0f74bd126
Karun Sandhu <karun@sndh.dev>
2026-04-29 20:39:18 +0200

    desktop: switch back to gnome-keyring

---
+++ modules/desktop/gnome-keyring.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules = {
+    nixos.desktop = {
+      services.gnome.gnome-keyring.enable = true;
+      security.pam.services = {
+        greetd.enableGnomeKeyring = true;
+        greetd-password.enableGnomeKeyring = true;
+        login.enableGnomeKeyring = true;
+      };
+    };
+
+    homeManager.desktop =
+      { pkgs, ... }:
+      {
+        home.packages = builtins.attrValues { inherit (pkgs) seahorse; };
+      };
+  };
+}

2e303fd58af49081568b007361ab4b09c6bac51b
Karun Sandhu <karun@sndh.dev>
2026-05-01 21:01:53 +0200

    claude-code: use up to date package name

--- modules/claude-code.nix
+++ modules/claude-code.nix
@@ -3,7 +3,7 @@
   flake.modules.homeManager.claude-code =
     { pkgs, ... }:
     {
-      home.packages = [ inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code-acp ];
+      home.packages = [ inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-agent-acp ];
       programs.claude-code = {
         enable = true;
         package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code;

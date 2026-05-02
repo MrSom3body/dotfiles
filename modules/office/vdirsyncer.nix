5a98436ffc3b9ef1a6da60ef7d17aa36ee0f70cb
Karun Sandhu <karun@sndh.dev>
2026-05-02 10:40:00 +0200

    vdirsyncer: refactor

--- modules/office/vdirsyncer.nix
+++ modules/office/vdirsyncer.nix
@@ -10,17 +10,18 @@
       programs.vdirsyncer.enable = true;
       services.vdirsyncer.enable = true;
 
-      systemd.user.services.vdirsyncer.Unit.After = [ "sops-nix.service" ];
-
-      systemd.user.services.vdirsyncer.Service = {
-        Restart = "on-failure";
-        StartLimitBurst = 2;
-        ExecStopPost = pkgs.writeShellScript "stop-post" ''
-          # When it requires a discovery
-          if [ "$SERVICE_RESULT" == "exit-code" ]; then
-            ${lib.getExe config.services.vdirsyncer.package} discover --no-list
-          fi
-        '';
+      systemd.user.services.vdirsyncer = {
+        Unit.After = [ "sops-nix.service" ];
+        Service = {
+          Restart = "on-failure";
+          StartLimitBurst = 2;
+          ExecStopPost = pkgs.writeShellScript "stop-post" ''
+            # When it requires a discovery
+            if [ "$SERVICE_RESULT" == "exit-code" ]; then
+              ${lib.getExe config.services.vdirsyncer.package} discover --no-list
+            fi
+          '';
+        };
       };
     };
 }

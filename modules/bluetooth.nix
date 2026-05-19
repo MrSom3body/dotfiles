60671b2fb6fa997df0feabc01d16c3dd55470cec
Karun Sandhu <karun@sndh.dev>
2026-05-19 17:59:11 +0200

    bluetooth: add kernel patch to unbork bluetooth

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,16 +1,29 @@
 {
   flake.modules = {
-    nixos.bluetooth = {
-      hardware.bluetooth = {
-        enable = true;
-      };
+    nixos.bluetooth =
+      { pkgs, ... }:
+      {
+        # TODO remove when https://github.com/NixOS/nixpkgs/issues/521528 gets resolved
+        boot.kernelPatches = [
+          {
+            name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+            patch = pkgs.fetchurl {
+              url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+              hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+            };
+          }
+        ];
 
-      services.blueman = {
-        enable = true;
-        # TODO remove when https://github.com/NixOS/nixpkgs/issues/514705 gets resolved
-        withApplet = false;
+        hardware.bluetooth = {
+          enable = true;
+        };
+
+        services.blueman = {
+          enable = true;
+          # TODO remove when https://github.com/NixOS/nixpkgs/issues/514705 gets resolved
+          withApplet = false;
+        };
       };
-    };
 
     homeManager.bluetooth = {
       services.blueman-applet.enable = true;

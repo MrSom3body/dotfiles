a422a93a8be6c1cb8e8d326fbab0f0da5fb1d30f
Karun Sandhu <karun@sndh.dev>
2026-05-26 12:25:48 +0200

    bluetooth: remove kernel patch

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,22 +1,9 @@
 {
   flake.modules = {
-    nixos.bluetooth =
-      { pkgs, ... }:
-      {
-        # TODO remove when https://github.com/NixOS/nixpkgs/issues/521528 gets resolved
-        boot.kernelPatches = [
-          {
-            name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
-            patch = pkgs.fetchurl {
-              url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
-              hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
-            };
-          }
-        ];
-
-        hardware.bluetooth.enable = true;
-        services.blueman.enable = true;
-      };
+    nixos.bluetooth = {
+      hardware.bluetooth.enable = true;
+      services.blueman.enable = true;
+    };
 
     homeManager.bluetooth = {
       services.blueman-applet.enable = true;

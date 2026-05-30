8373a81309f08d7505f7b57f3af5281a2c9c114e
Karun Sandhu <karun@sndh.dev>
2026-05-30 14:02:06 +0200

    gemini-cli -> antigravity-cli

---
+++ modules/antigravity-cli.nix
@@ -0,0 +1,57 @@
+{ inputs, ... }:
+# let
+#   inherit (config.flake.meta) programs;
+# in
+{
+  flake.modules.homeManager.antigravity-cli =
+    { pkgs, ... }:
+    {
+      home.packages = [ inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.antigravity ];
+      # programs.gemini-cli = {
+      #   enable = true;
+      #   package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.gemini-cli;
+      #   settings = {
+      #     ui = {
+      #       hideBanner = true;
+      #       footer.hideContextPercentage = false;
+      #       inlineThinkingMode = "full";
+      #       showCitations = true;
+      #       showModelInfoInChat = true;
+      #       showStatusInTitle = true;
+      #       useAlternateBuffer = true;
+      #     };
+
+      #     general = {
+      #       preferredEditor = programs.editor;
+      #       previewFeatures = true;
+      #       sessionRetention = {
+      #         enabled = true;
+      #         maxAge = "30d";
+      #         maxCount = 100;
+      #       };
+      #       vimMode = true;
+      #       checkpointing.enabled = true;
+      #     };
+      #     ide.enabled = true;
+      #     context = {
+      #       fileName = [
+      #         "AGENTS.md"
+      #         "GEMINI.md"
+      #       ];
+      #       discoveryMaxDirs = 1000;
+      #     };
+      #     tools.shell.showColor = true;
+      #     security = {
+      #       disableYoloMode = true;
+      #       auth.selectedType = "oauth-personal";
+      #     };
+      #     experimental = {
+      #       enableAgents = true;
+      #       plan = true;
+      #     };
+
+      #     privacy.usageStatisticsEnabled = false;
+      #   };
+      # };
+    };
+}

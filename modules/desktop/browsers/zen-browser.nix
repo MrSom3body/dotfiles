b58e3737a5d716ac26d20e2b87c78ce853718c44
Karun Sandhu <karun@sndh.dev>
2026-05-31 11:44:29 +0200

    zen-browser: add mods

--- modules/desktop/browsers/zen-browser.nix
+++ modules/desktop/browsers/zen-browser.nix
@@ -15,6 +15,14 @@
           name = "default";
           isDefault = true;
           inherit (defaults) settings search;
+          mods = [
+            "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
+            "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+            "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
+            "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+            "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
+            "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e" # Quietify
+          ];
         };
       };
     };

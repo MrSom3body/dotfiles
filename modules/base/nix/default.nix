3d6f23ba651af3ccb4b6938c0ac1ecc96b955121
Karun Sandhu <karun@sndh.dev>
2026-06-03 21:18:06 +0200

    nix: set unfree env var

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -5,56 +5,72 @@
   ...
 }:
 {
-  flake.modules.nixos.nixos =
-    { config, pkgs, ... }:
-    {
-      environment.systemPackages = [ pkgs.git ];
-
-      programs.nh = {
-        enable = true;
-        clean = {
-          enable = true;
-          extraArgs = "--keep-since 3d --keep 3";
-        };
-        flake = "/home/karun/dotfiles";
-      };
-
-      nix =
-        let
-          flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
-        in
-        {
-          # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-          registry = lib.mapAttrs (_: v: { flake = v; }) flakeInputs;
-          nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
-          channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
-
-          settings = {
-            accept-flake-config = lib.mkForce false;
-            log-lines = lib.mkDefault 25; # more log lines
-
-            cores = 0;
-            max-jobs = "auto";
-
-            builders-use-substitutes = true;
-            experimental-features = [
-              "nix-command"
-              "flakes"
-              "auto-allocate-uids"
-              "configurable-impure-env"
-            ];
-            impure-env = [ "NIXPKGS_ALLOW_UNFREE" ];
-
-            keep-derivations = true;
-            keep-outputs = true;
-
-            trusted-users = [ "@wheel" ];
-          };
-        };
-
-      nixpkgs = {
+  flake.modules =
+    let
+      sharedNixpkgs = {
         config.allowUnfree = true;
         overlays = [ self.overlays.default ];
       };
+    in
+    {
+      nixos.nixos =
+        { config, pkgs, ... }:
+        {
+          environment.systemPackages = [ pkgs.git ];
+
+          programs.nh = {
+            enable = true;
+            clean = {
+              enable = true;
+              extraArgs = "--keep-since 3d --keep 3";
+            };
+            flake = "/home/karun/dotfiles";
+          };
+
+          nix =
+            let
+              flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
+            in
+            {
+              # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+              registry = lib.mapAttrs (_: v: { flake = v; }) flakeInputs;
+              nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
+              channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
+
+              settings = {
+                accept-flake-config = lib.mkForce false;
+                log-lines = lib.mkDefault 25; # more log lines
+
+                cores = 0;
+                max-jobs = "auto";
+
+                builders-use-substitutes = true;
+                experimental-features = [
+                  "nix-command"
+                  "flakes"
+                  "auto-allocate-uids"
+                  "configurable-impure-env"
+                ];
+                impure-env = [ "NIXPKGS_ALLOW_UNFREE" ];
+
+                keep-derivations = true;
+                keep-outputs = true;
+
+                trusted-users = [ "@wheel" ];
+              };
+            };
+
+          nixpkgs = sharedNixpkgs;
+        };
+
+      homeManager.homeManager =
+        {
+          osConfig ? null,
+          ...
+        }:
+        {
+          nixpkgs = lib.mkIf (osConfig == null || !osConfig.home-manager.useGlobalPkgs) sharedNixpkgs;
+          home.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
+        };
     };
 }

f1cab0cf48e89f66135c8e0e5cd3abd941d6b619
Karun Sandhu <karun@sndh.dev>
2026-07-02 19:34:56 +0200

    zswap: switch to zstd compressor

--- modules/base/system/zswap.nix
+++ modules/base/system/zswap.nix
@@ -2,7 +2,7 @@
   flake.modules.nixos.nixos = {
     boot.kernel.sysfs.module.zswap.parameters = {
       enabled = 1;
-      compressor = "lz4";
+      compressor = "zstd";
       max_pool_percent = 20;
       shrinker_enabled = 1;
     };

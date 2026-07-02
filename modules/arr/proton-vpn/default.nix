fc8080be9375eb86985a178b4cf0049597620cd3
Karun Sandhu <karun@sndh.dev>
2026-07-03 01:08:24 +0200

    arr/proton-vpn: limit vpn to transmission

--- modules/arr/proton-vpn/default.nix
+++ modules/arr/proton-vpn/default.nix
@@ -3,7 +3,10 @@
     { config, ... }:
     let
       active = "de-834";
-      secret = "protonvpn-wg-${active}";
+      wgSecret = field: {
+        key = "protonvpn/${active}/${field}";
+        sopsFile = ../../../secrets/protonvpn.yaml;
+      };
     in
     {
       imports = [
@@ -13,8 +16,31 @@
         })
       ];
 
-      sops.secrets.${secret}.sopsFile = ../../../secrets/protonvpn.yaml;
+      sops = {
+        secrets = {
+          wg-private-key = wgSecret "private-key";
+          wg-public-key = wgSecret "public-key";
+          wg-endpoint = wgSecret "endpoint";
+        };
 
-      networking.wg-quick.interfaces.protonvpn.configFile = config.sops.secrets.${secret}.path;
+        templates."protonvpn.conf".content = ''
+          [Interface]
+          PrivateKey = ${config.sops.placeholder.wg-private-key}
+          Address = 10.2.0.2/32
+          Table = off
+          PostUp = ip route add 10.2.0.1/32 dev %i
+          PostUp = ip rule add from 10.2.0.2 lookup 51820
+          PostUp = ip route add default dev %i table 51820
+          PreDown = ip rule del from 10.2.0.2 lookup 51820
+
+          [Peer]
+          PublicKey = ${config.sops.placeholder.wg-public-key}
+          AllowedIPs = 0.0.0.0/0, ::/0
+          Endpoint = ${config.sops.placeholder.wg-endpoint}:51820
+          PersistentKeepalive = 25
+        '';
+      };
+
+      networking.wg-quick.interfaces.protonvpn.configFile = config.sops.templates."protonvpn.conf".path;
     };
 }

5d34e3daec6df08b690fc315e1106656d5451a38
Karun Sandhu <karun@sndh.dev>
2026-07-09 12:04:48 +0200

    ollama: use port defined in services

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,8 +1,13 @@
+{ config, ... }:
+let
+  inherit (config.flake) meta;
+in
 {
   flake.modules.nixos.ollama = {
     services.ollama = {
       enable = true;
       syncModels = true;
+      port = meta.services.karakeep.port;
     };
   };
 }

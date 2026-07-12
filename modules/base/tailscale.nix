3e3723558ca52677c6947bd1467952ebc88c6daa
Karun Sandhu <karun@sndh.dev>
2026-07-12 21:08:35 +0200

    tailscale: don't forcefully set nftables mode

--- modules/base/tailscale.nix
+++ modules/base/tailscale.nix
@@ -7,8 +7,6 @@
         allowedUDPPorts = [ config.services.tailscale.port ];
       };
 
-      systemd.services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
-
       services = {
         tailscale = {
           enable = true;

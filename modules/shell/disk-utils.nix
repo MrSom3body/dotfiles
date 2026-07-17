6dfecb984b9cf3374fa12c17bb39c9443478e81e
Karun Sandhu <karun@sndh.dev>
2026-07-17 14:38:06 +0200

    shell/disk-utils: replace dust with dua

--- modules/shell/disk-utils.nix
+++ modules/shell/disk-utils.nix
@@ -1,11 +1,5 @@
 {
   flake.modules.homeManager.shell = { pkgs, ... }: {
-    xdg.configFile."dust/config.toml".text =
-      # toml
-      ''
-        reverse=false
-      '';
-
-    home.packages = builtins.attrValues { inherit (pkgs) dust sd; };
+    home.packages = builtins.attrValues { inherit (pkgs) dua sd; };
   };
 }

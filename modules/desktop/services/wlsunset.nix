dca1183006ac160d840c893d13a6d8bd52df9f01
Karun Sandhu <karun@sndh.dev>
2026-07-20 21:41:20 +0200

    wlsunset: add comment about duration

--- modules/desktop/services/wlsunset.nix
+++ modules/desktop/services/wlsunset.nix
@@ -4,6 +4,7 @@
       enable = true;
       sunset = "21:00";
       sunrise = "06:00";
+      # duration = 1800; # TODO uncomment when https://github.com/nix-community/home-manager/pull/9691 gets merged
       temperature = {
         day = 6500;
         night = 3700;

f16fbc9399e7dc43813bf78646a482318b85bd0e
Karun Sandhu <karun@sndh.dev>
2026-07-26 22:09:35 +0200

    iso: force disable networkmanager

--- modules/iso/overrides.nix
+++ modules/iso/overrides.nix
@@ -7,6 +7,7 @@ in
     system.autoUpgrade.enable = mkForce false; # really not needed on an ISO
 
     # overrides needed for my config to work with the iso template
+    networking.networkmanager.enable = mkForce false;
     boot.supportedFilesystems.zfs = mkForce false;
     security = {
       sudo.enable = false;

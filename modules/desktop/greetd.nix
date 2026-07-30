fb1d70f3f234ea44fd40af671d02cd61b68a6d48
Karun Sandhu <karun@sndh.dev>
2026-07-30 09:29:01 +0200

    greetd: programs.regreet -> services.desktopManager.regreet

--- modules/desktop/greetd.nix
+++ modules/desktop/greetd.nix
@@ -7,14 +7,14 @@
           terminal.vt = 1;
         };
       };
+
+      displayManager.regreet = {
+        enable = true;
+        settings.skip_selection = true;
+      };
     };
 
     security.pam.services.greetd.fprintAuth = false;
     security.pam.services.greetd-password.fprintAuth = false;
-
-    programs.regreet = {
-      enable = true;
-      settings.skip_selection = true;
-    };
   };
 }

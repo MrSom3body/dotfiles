ae868160a1b37ed3cc52f94901771b673eee7477
Karun Sandhu <karun@sndh.dev>
2026-08-01 12:40:25 +0200

    paperless: fetch groups from kanidm and drop secrets for oauth

--- modules/paperless.nix
+++ modules/paperless.nix
@@ -4,40 +4,7 @@ let
 in
 {
   flake.modules.nixos.paperless = { config, ... }: {
-    sops.secrets = {
-      paperless-admin-pass.sopsFile = ../secrets/paperless.yaml;
-      paperless-oauth-secret.sopsFile = ../secrets/paperless.yaml;
-    };
-
-    sops.templates."paperless-providers.env" = {
-      owner = "paperless";
-      content = ''
-        PAPERLESS_SOCIALACCOUNT_PROVIDERS='${
-          builtins.toJSON {
-            openid_connect = {
-              OAUTH_PKCE_ENABLED = true;
-              APPS = [
-                {
-                  provider_id = "kanidm";
-                  name = "som3sso";
-                  client_id = "paperless";
-                  settings = {
-                    oauth_pkce_enabled = true;
-                    server_url = "${meta.services.kanidm.url}/oauth2/openid/paperless/.well-known/openid-configuration";
-                    fetch_userinfo = true;
-                  };
-                }
-              ];
-              SCOPE = [
-                "openid"
-                "profile"
-                "email"
-              ];
-            };
-          }
-        }'
-      '';
-    };
+    sops.secrets.paperless-admin-pass.sopsFile = ../secrets/paperless.yaml;
 
     services = {
       caddy.virtualHosts."${meta.services.paperless.domain}" = {
@@ -48,7 +15,6 @@ in
 
       paperless = {
         enable = true;
-        environmentFile = config.sops.templates."paperless-providers.env".path;
         port = meta.services.paperless.port;
         domain = meta.services.paperless.domain;
         passwordFile = config.sops.secrets.paperless-admin-pass.path;
@@ -58,6 +24,30 @@ in
           PAPERLESS_OCR_LANGUAGE = "deu+eng";
 
           PAPERLESS_APPS = "allauth.socialaccount.providers.openid_connect";
+          PAPERLESS_SOCIAL_ACCOUNT_SYNC_GROUPS = true;
+          PAPERLESS_SOCIALACCOUNT_PROVIDERS = {
+            openid_connect = {
+              OAUTH_PKCE_ENABLED = true;
+              APPS = [
+                {
+                  provider_id = "kanidm";
+                  name = "som3sso";
+                  client_id = "paperless";
+                  secret = "";
+                  settings = {
+                    server_url = "${meta.services.kanidm.url}/oauth2/openid/paperless/.well-known/openid-configuration";
+                    fetch_userinfo = true;
+                  };
+                }
+              ];
+              SCOPE = [
+                "openid"
+                "profile"
+                "email"
+                "groups"
+              ];
+            };
+          };
         };
       };
     };

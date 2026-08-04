fe4471023a3f0ebf2d0174e1754536283a48134a
Karun Sandhu <karun@sndh.dev>
2026-08-04 14:16:35 +0200

    hyprland/binds/workspaces: allow moving workspaces between monitors

--- modules/hyprland/hyprland/binds/workspaces.nix
+++ modules/hyprland/hyprland/binds/workspaces.nix
@@ -19,6 +19,34 @@
             { description = "Switch to next workspace"; }
           ];
         }
+        {
+          _args = [
+            "SUPER + CTRL + Left"
+            (lua ''hl.dsp.workspace.move({ monitor = "l" })'')
+            { description = "Move workspace to left monitor"; }
+          ];
+        }
+        {
+          _args = [
+            "SUPER + CTRL + Right"
+            (lua ''hl.dsp.workspace.move({ monitor = "r" })'')
+            { description = "Move workspace to right monitor"; }
+          ];
+        }
+        {
+          _args = [
+            "SUPER + CTRL + Up"
+            (lua ''hl.dsp.workspace.move({ monitor = "u" })'')
+            { description = "Move workspace to top monitor"; }
+          ];
+        }
+        {
+          _args = [
+            "SUPER + CTRL + Down"
+            (lua ''hl.dsp.workspace.move({ monitor = "d" })'')
+            { description = "Move workspace to bottom monitor"; }
+          ];
+        }
       ]
       ++ builtins.concatLists (
         builtins.genList (
@@ -39,6 +67,18 @@
                 "SUPER + SHIFT + code:1${toString i}"
                 (lua "hl.dsp.window.move({ workspace = ${toString ws} })")
                 { description = "Move focused window to workspace ${toString ws}"; }
+              ];
+            }
+            {
+              _args = [
+                "SUPER + ALT + code:1${toString i}"
+                (lua ''
+                  function()
+                    hl.dispatch(hl.dsp.workspace.move({ workspace = ${toString ws}, monitor = "current" }))
+                    hl.dispatch(hl.dsp.focus({ workspace = ${toString ws} }))
+                  end
+                '')
+                { description = "Move workspace ${toString ws} to current monitor"; }
               ];
             }
           ]

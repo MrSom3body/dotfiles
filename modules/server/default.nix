15fdfc04b317509ffc8bf5c9e55fcf32931df256
Karun Sandhu <karun@sndh.dev>
2026-08-06 21:37:01 +0200

    server: block all ports

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,13 +1,1 @@
-{
-  flake.modules.nixos.server = { lib, ... }: {
-    networking = {
-      firewall = {
-        enable = lib.mkForce true;
-        allowedTCPPorts = [
-          80
-          443
-        ];
-      };
-    };
-  };
-}
+{ flake.modules.nixos.server = { lib, ... }: { networking.firewall.enable = lib.mkForce true; }; }

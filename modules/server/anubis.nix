8ef74e277058421fe359c3ec92e5f5bbdc9404a1
Karun Sandhu <karun@sndh.dev>
2026-08-06 22:41:06 +0200

    anubis: allow cloudflared to access the unix sockets

--- modules/server/anubis.nix
+++ modules/server/anubis.nix
@@ -1,11 +1,19 @@
 {
   flake.modules.nixos.server =
-    { config, ... }:
+    { lib, config, ... }:
     let
       cfg = config.services.anubis.defaultOptions;
-      caddyCfg = config.services.caddy;
     in
     {
-      users.users."${caddyCfg.user}".extraGroups = [ cfg.group ];
+      config = lib.mkIf (config.services.anubis.instances != { }) {
+        users.users."${config.services.caddy.user}".extraGroups = [ cfg.group ];
+
+        systemd.services = lib.mapAttrs' (
+          name: _:
+          lib.nameValuePair "cloudflared-tunnel-${name}" {
+            serviceConfig.SupplementaryGroups = [ cfg.group ];
+          }
+        ) config.services.cloudflared.tunnels;
+      };
     };
 }

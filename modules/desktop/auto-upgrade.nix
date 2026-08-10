5f97e6d8c1cd1ac2bdc8d8829d696c2f0251dae7
Karun Sandhu <karun@sndh.dev>
2026-08-10 09:56:57 +0200

    dekstop/auto-upgrade: make sure that the system has a real connection before trying update

--- modules/desktop/auto-upgrade.nix
+++ modules/desktop/auto-upgrade.nix
@@ -1,11 +1,20 @@
-{ config, ... }: {
-  flake.modules.nixos.desktop = {
+{ config, ... }:
+let
+  inherit (config) flake;
+in
+{
+  flake.modules.nixos.desktop = { config, ... }: {
     system.autoUpgrade = {
       enable = true;
-      flake = config.flake.meta.uri;
+      flake = flake.meta.uri;
       upgrade = false;
       operation = "boot";
       dates = "05:00";
     };
+
+    systemd.services.nixos-upgrade.preStart = ''
+      echo "Waiting for systemd-networkd to have an online interface..."
+      ${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any --dns -q
+    '';
   };
 }

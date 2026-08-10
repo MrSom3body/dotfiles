c38da575c511fd417cd3b3f46e5a7dd5c66e0291
Karun Sandhu <karun@sndh.dev>
2026-08-10 13:47:06 +0200

    karakeep: set connection timeout to 150s

--- modules/karakeep.nix
+++ modules/karakeep.nix
@@ -49,6 +49,7 @@ in
             # ai
             OPENAI_API_KEY = "ollama";
             OPENAI_BASE_URL = "http://127.0.0.1:${toString ollamaCfg.port}/v1";
+            INFERENCE_JOB_TIMEOUT_SEC = "150";
             INFERENCE_TEXT_MODEL = llmModel;
             INFERENCE_IMAGE_MODEL = llmModel;
             EMBEDDING_TEXT_MODEL = embeddingModel;

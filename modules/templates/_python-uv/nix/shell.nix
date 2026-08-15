f2fec0b58fed12732e885c6bff985cdf3f944270
Karun Sandhu <karun@sndh.dev>
2026-08-15 14:47:07 +0200

    templates/python-uv: stdenv.isLinux -> stdenv.hostPlatform.isLinux

--- modules/templates/_python-uv/nix/shell.nix
+++ modules/templates/_python-uv/nix/shell.nix
@@ -14,7 +14,7 @@
       # Force uv to use nixpkgs Python interpreter
       UV_PYTHON = python.interpreter;
     }
-    // lib.optionalAttrs pkgs.stdenv.isLinux {
+    // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
       # Python libraries often load native shared objects using dlopen(3).
       # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
       LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;

076576e8ce99bb70c772be330dfebade63ca5923
Karun Sandhu <karun@sndh.dev>
2026-08-24 18:45:12 +0200

    greetd: switch from regreet to tuigreet

--- modules/desktop/greetd.nix
+++ modules/desktop/greetd.nix
@@ -1,20 +1,45 @@
-{
-  flake.modules.nixos.desktop = {
-    services = {
-      greetd = {
-        enable = true;
-        settings = {
-          terminal.vt = 1;
+{ lib, ... }: {
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+    let
+      tuigreet-config = (pkgs.formats.toml { }).generate "tuigreet-config.toml" {
+        display.show_time = true;
+        user_menu.enabled = true;
+        background = {
+          kind = "doom";
+          doom.height = 4;
+        };
+        secret.mode = "characters";
+        layout = {
+          width = 60;
+          window_padding = 2;
+          container_padding = 1;
+          prompt_padding = 1;
+        };
+
+        remember = {
+          username = true;
+          user_session = true;
+        };
+      };
+    in
+    {
+      services = {
+        greetd = {
+          enable = true;
+          settings = {
+            default_session = {
+              user = "greeter";
+              command = "${lib.getExe' pkgs.tuigreet "tuigreet"} --config ${tuigreet-config}";
+            };
+            terminal.vt = 1;
+          };
         };
       };
 
-      displayManager.regreet = {
-        enable = true;
-        settings.skip_selection = true;
-      };
-    };
+      systemd.tmpfiles.rules = [ "d /var/cache/tuigreet 0755 greeter greeter" ];
 
-    security.pam.services.greetd.fprintAuth = false;
-    security.pam.services.greetd-password.fprintAuth = false;
-  };
+      security.pam.services.greetd.fprintAuth = false;
+      security.pam.services.greetd-password.fprintAuth = false;
+    };
 }

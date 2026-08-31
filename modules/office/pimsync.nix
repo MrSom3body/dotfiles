687b9db0aa7d3cc6eeb8a6532eb316808377a600
Karun Sandhu <karun@sndh.dev>
2026-08-31 10:18:25 +0200

    pimsync: setup restart on service failure

--- modules/office/pimsync.nix
+++ modules/office/pimsync.nix
@@ -2,5 +2,10 @@
   flake.modules.homeManager.office = {
     programs.pimsync.enable = true;
     services.pimsync.enable = true;
+
+    systemd.user.services.pimsync.Service = {
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
   };
 }

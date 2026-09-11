6571e183c96abd556d3d4d4dc4bcbf007b58ff22
Karun Sandhu <karun@sndh.dev>
2026-09-11 11:34:17 +0200

    overlays: add tsukimi

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -3,6 +3,31 @@ let
   nix-topology = inputs.nix-topology.overlays.default;
 
   modifications = final: prev: {
+    # TODO remove when https://github.com/NixOS/nixpkgs/pull/558436 gets merged
+    tsukimi = prev.tsukimi.overrideAttrs (
+      finalAttrs: previousAttrs: {
+        version = "26.9.1";
+        src = prev.fetchFromGitHub {
+          owner = "tsukinaha";
+          repo = "tsukimi";
+          tag = "v${finalAttrs.version}";
+          hash = "sha256-YhagPMD5nSzLCWQc+LjStezMvzapxw+an1pxN2AdZPM=";
+        };
+        cargoDeps = prev.rustPlatform.fetchCargoVendor {
+          inherit (finalAttrs) pname version src;
+          hash = "sha256-EQyG2ZSAHP5Tc6WSWyNa950en4SX+kbzgUUKeVnAswg=";
+        };
+        nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [
+          prev.blueprint-compiler
+          prev.libglycin.patchVendorHook
+        ];
+        buildInputs = previousAttrs.buildInputs ++ [
+          prev.libglycin
+          prev.glycin-loaders
+        ];
+      }
+    );
+
     obsidian = prev.obsidian.overrideAttrs (oldAttrs: {
       postInstall = (oldAttrs.postInstall or "") + ''
         wrapProgram $out/bin/obsidian \

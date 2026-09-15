56854627c00c07688cc8d429014a495da9e8eef8
Karun Sandhu <karun@sndh.dev>
2026-09-15 10:46:13 +0200

    hyprland/gestures: add gestures for moving windows
    now uses 4 finger swipe for workspace/column movement & 3 finger swipe for window movement

--- modules/hyprland/hyprland/gestures.nix
+++ modules/hyprland/hyprland/gestures.nix
@@ -3,27 +3,43 @@
     { lib, config, ... }:
     let
       inherit (config.wayland.windowManager.hyprland) layout;
-      lua = lib.generators.mkLuaInline;
+      luaFunc =
+        func:
+        lib.generators.mkLuaInline ''
+          function()
+            ${func}
+          end
+        '';
     in
     {
       wayland.windowManager.hyprland.settings = {
         gesture = [
           {
             fingers = 4;
-            direction = "pinch";
-            action = lua ''function() hl.exec_cmd("loginctl lock-session") end'';
+            direction = "pinchin";
+            action = luaFunc ''hl.exec_cmd("loginctl lock-session")'';
           }
           {
             fingers = 3;
+            direction = "swipe";
+            action = "move";
+          }
+          {
+            fingers = 4;
             direction = "horizontal";
             action = if layout == "scrolling" then "scroll_move" else "workspace";
           }
         ]
         ++ lib.optionals (layout == "scrolling") [
           {
-            fingers = 3;
+            fingers = 4;
             direction = "vertical";
             action = "workspace";
+          }
+          {
+            fingers = 3;
+            direction = "pinchout";
+            action = luaFunc ''hl.dsp.layout("promote")'';
           }
         ];
       };
